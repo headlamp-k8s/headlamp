e5277035a85280372c4df1036f94f4b5c0543cf0
Joaquim Rocha <joaquim@kinvolk.io>
2019-11-11 17:07:42 +0100

    frontend: Add common components
    These common components come from the Nebraska project, and are useful
    in this project too. Ideally we will split them out into their own
    repository so they are shared across projects more independently, but
    for now it is okay to keep them here.

---
+++ frontend/src/components/common/EmptyContent.js
@@ -0,0 +1,17 @@
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+export default function Empty(props) {
+  return (
+    <Box padding={2}>
+      {React.Children.map(props.children, child => {
+        if (typeof child === 'string') {
+          return <Typography color="textSecondary" align="center">{child}</Typography>
+        }
+        return child;
+      })
+      }
+    </Box>
+  );
+}
---
+++ frontend/src/components/common/SectionHeader.js
@@ -0,0 +1,36 @@
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+const useStyles = makeStyles(theme => ({
+  sectionHeader: {
+    padding: '1em',
+  },
+}));
+
+export default function SectionHeader(props) {
+  const classes = useStyles();
+  let actions = props.actions || [];
+
+  return (<Grid
+            container
+            alignItems="flex-start"
+            justify="space-between"
+            className={classes.sectionHeader}
+          >
+            {props.title &&
+              <Grid item>
+                <Typography variant="h5">{props.title}</Typography>
+              </Grid>
+            }
+            {actions &&
+              actions.map((action, i) =>
+                <Grid item key={i}>
+                  {action}
+                </Grid>
+              )
+            }
+          </Grid>
+  );
+}
