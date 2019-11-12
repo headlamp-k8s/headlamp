98e50235e20141c1c552ca76c0ecc77d7ca40ada
Joaquim Rocha <joaquim@kinvolk.io>
2019-11-12 09:40:40 +0100

    frontend: Show events table in the cluster overview
    This is implemented in a basic way, both in terms of UI and in how the
    API is connected. It will be improved in future commits.

--- frontend/src/components/ClusterOverview.js
+++ frontend/src/components/ClusterOverview.js
@@ -1,25 +1,67 @@
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 import React from 'react';
 import api from '../lib/api';
-import Typography from '@material-ui/core/Typography';
+import SectionHeader from './common/SectionHeader';
+import SimpleTable from './common/SimpleTable';
 
 export default function ClusterOverview(props) {
-    const [pods, setPods] = React.useState(null);
-    const [events, setEvents] = React.useState(null);
-    const [nodes, setNodes] = React.useState(null);
+  const [pods, setPods] = React.useState(null);
+  const [eventsData, setEventsData] = React.useState(null);
+  const [nodes, setNodes] = React.useState(null);
 
-    React.useEffect(() => {
-        api.pod.list(null, setPods);
-        api.event.list(null, setEvents);
-        api.node.list(setNodes);
-    },
-    []);
+  function setEvents(events) {
+    const data = events.map(event => {
+      console.log(event);
+      return {
+        kind: event.involvedObject.kind,
+        name: event.involvedObject.name,
+        time: event.metadata.creationTimestamp,
+        reason: event.reason,
+        message: event.message,
+      };
+    });
+    setEventsData(data);
+  }
 
-    return (
-        <Typography paragraph>
-            Hello
-            {/* {JSON.stringify(pods)} */}
-            {JSON.stringify(events)}
-            {JSON.stringify(nodes)}
-        </Typography>
-    );
+  React.useEffect(() => {
+    api.pod.list(null, setPods);
+    api.event.list(null, setEvents);
+    api.node.list(setNodes);
+  },
+  []);
+
+  return (
+    <Paper>
+      <SectionHeader title="Events" />
+      <Box margin={1}>
+        <SimpleTable
+          rowsPerPage={[15, 25, 50]}
+          columns={[
+            {
+              label: 'Type',
+              datum: 'kind'
+            },
+            {
+              label: 'Name',
+              datum: 'name',
+            },
+            {
+              label: 'Time',
+              datum: 'time',
+            },
+            {
+              label: 'Reason',
+              datum: 'reason',
+            },
+            {
+              label: 'Message',
+              datum: 'message',
+            }
+          ]}
+          data={eventsData}
+        />
+      </Box>
+    </Paper>
+  );
 }
