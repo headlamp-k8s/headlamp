772f61c7b7a56f2bc2786156189c93fd4621c1a0
Joaquim Rocha <joaquim@kinvolk.io>
2019-11-20 17:46:06 +0100

    frontend: Add a memory usage chart to the Cluster Overview
    It is useful to quickly understand how much memory is being used and
    available in the Cluster, so this patch adds a new chart to the Cluster
    Overview that displays that information.

---
+++ frontend/src/components/cluster/Charts.js
@@ -0,0 +1,63 @@
+import _ from 'lodash';
+import React from 'react';
+import { parseRam, TO_GB } from '../../lib/units';
+import { PercentageCircle } from '../common/Chart';
+
+export function MemoryCircularChart(props) {
+  const [usedMemory, totalMemory] = getMemoryUsage(props);
+
+  function filterMetrics(items, metrics) {
+    if (!items || !metrics)
+      return [];
+
+    const names = items.map(({metadata}) => metadata.name);
+    return metrics.filter(item => names.includes(item.metadata.name));
+  }
+
+  function getLabel() {
+    if (totalMemory === 0) {
+      return '…';
+    }
+    return `${(usedMemory / totalMemory * 100).toFixed(1)} %`;
+  }
+
+  function getMemoryUsage() {
+    const { nodes, nodesMetrics } = props;
+
+    if (!nodes)
+      return [0, 0];
+
+    const nodeMetrics = filterMetrics(nodes, nodesMetrics);
+    const usedMemory = _.sumBy(nodeMetrics, item => parseRam(item.usage.memory) / TO_GB);
+    const totalMemory = _.sumBy(nodes, item => parseRam(item.status.capacity.memory) / TO_GB);
+
+    return [usedMemory, totalMemory];
+  }
+
+  function makeData() {
+    return [
+      {
+        name: 'used',
+        value: usedMemory,
+      }
+    ];
+  }
+
+  function getLegend() {
+    if (totalMemory === 0) {
+      return null;
+    }
+
+    return `${usedMemory.toFixed(2)} / ${totalMemory.toFixed(2)} GB`;
+  }
+
+  return (
+    <PercentageCircle
+      title="Memory Usage"
+      data={makeData()}
+      total={totalMemory}
+      label={getLabel()}
+      legend={getLegend()}
+    />
+  );
+}
