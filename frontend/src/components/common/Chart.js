4d9afc48074777b469294cb7ad6f3b356661a952
Joaquim Rocha <joaquim@kinvolk.io>
2019-11-20 14:58:34 +0100

    frontend: Add basic PercentageCircle
    This new chart will be used to display percentage values in a pie chart.
    
    These changes introduce a new dependency: Recharts.

---
+++ frontend/src/components/common/Chart.js
@@ -0,0 +1,106 @@
+import Box from '@material-ui/core/Box';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import { Label, Pie, PieChart } from 'recharts';
+
+const useStyle = makeStyles(theme => ({
+  title: {
+    textAlign: 'center',
+    fontSize: '1.2em',
+    flexGrow: 1,
+    fontWeight: 'bold',
+  },
+  legend: {
+    textAlign: 'center',
+    fontSize: '1.1em',
+    flexGrow: 1,
+  },
+  chart: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
+}));
+
+export function PercentageCircle(props) {
+  const theme = useTheme();
+  const classes = useStyle();
+  let {
+    data,
+    size=200,
+    dataKey='percentage',
+    label='',
+    title='',
+    legend=null,
+    total=100,
+  } = props;
+
+  const chartSize = size * .8;
+
+  function formatData() {
+    let filledValue = 0;
+    const formattedData = data.map(item => {
+      filledValue += item.value;
+
+      return {
+        percentage: item.value / total * 100,
+        ...item
+      };
+    });
+
+    const totalValue = {
+      name: 'total',
+      percentage: (total - filledValue) / total * 100,
+      fill: theme.palette.grey['300'],
+    };
+
+    return formattedData.concat(totalValue);
+  }
+
+  return (
+    <Box
+      justifyContent="center"
+      alignItems="center"
+      alignContent="center"
+      mx="auto"
+    >
+      {title &&
+        <Typography className={classes.title}>{title}</Typography>
+      }
+      <PieChart
+        cx={size / 2}
+        cy={size / 2}
+        width={chartSize}
+        height={chartSize}
+        className={classes.chart}
+      >
+        <Pie
+          data={formatData()}
+          // Center the chart
+          cx={chartSize / 2}
+          cy={chartSize / 2}
+          innerRadius={chartSize * .35}
+          outerRadius={chartSize * .4}
+          dataKey={dataKey}
+          // Start at the top
+          startAngle={90}
+          endAngle={-270}
+          fill={theme.palette.primary.main}
+        >
+          <Label
+            cx={chartSize / 2}
+            cy={chartSize / 2}
+            value={label}
+            position="center"
+            style={{
+              fontSize: `${chartSize * .15}px`,
+            }}
+          />
+        </Pie>
+      </PieChart>
+      {legend !== null &&
+        <Typography className={classes.legend}>{legend}</Typography>
+      }
+    </Box>
+  );
+}
