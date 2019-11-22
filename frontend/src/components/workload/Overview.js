a99ba335c899ea585c0f011b649c7e04f79551ef
Joaquim Rocha <joaquim@kinvolk.io>
2019-11-22 18:44:58 +0100

    frontend: Add CronJobs and ReplicaSets to the Workload overview

--- frontend/src/components/workload/Overview.js
+++ frontend/src/components/workload/Overview.js
@@ -42,6 +42,8 @@ export default function Overview() {
     api.daemonSet.list.bind(null, null, dispatch),
     api.deployment.list.bind(null, null, dispatch),
     api.job.list.bind(null, null, dispatch),
+    api.cronJob.list.bind(null, null, dispatch),
+    api.replicaSet.list.bind(null, null, dispatch),
     api.statefulSet.list.bind(null, null, dispatch),
   );
 
@@ -61,8 +63,16 @@ export default function Overview() {
       title: 'StatefulSets',
     },
     {
+      kind: 'ReplicaSet',
+      title: 'ReplicaSets',
+    },
+    {
       kind: 'Job',
       title: 'Jobs',
+    },
+    {
+      kind: 'CronJob',
+      title: 'CronJobs',
     },
   ];
 
