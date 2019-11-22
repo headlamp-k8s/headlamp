d508142fc06270b29b33fa640c944dd99aa0c94c
Joaquim Rocha <joaquim@kinvolk.io>
2019-11-22 12:05:32 +0100

    frontend: Add getPercentStr util function
    We should abstract code that creates common things like creating a
    percentage label/string. So this method does that.

--- frontend/src/lib/util.js
+++ frontend/src/lib/util.js
@@ -7,3 +7,13 @@ const TIME_AGO = new TimeAgo();
 export function timeAgo(date) {
   return TIME_AGO.format(new Date(date), 'time');
 }
+
+export function getPercentStr(value, total) {
+  if (total === 0) {
+    return null;
+  }
+  const percentage = value / total * 100;
+  const decimals = percentage % 10 > 0 ? 1 : 0;
+  return `${percentage.toFixed(decimals)} %`;
+
+}
