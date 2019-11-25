3501559645e014757fad9918fc51d1d6a4ac69b9
Joaquim Rocha <joaquim@kinvolk.io>
2019-11-25 11:06:32 +0100

    frontend: Add support for nested items in the Sidebar
    The sidebar so far supported just one single level of items, but this
    makes the organization of items difficult after a certain number that
    has already been reached. This patch thus adds the support for nested
    items to the sidebar component.
    
    The actual reorganization of the sidebar's items will be done in a later
    commit.

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -4,8 +4,9 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 import { getRoute } from '../lib/router';
+import Collapse from '@material-ui/core/Collapse';
 
 const DRAWER_WIDTH = 200;
 
@@ -89,6 +90,7 @@ const LIST_ITEMS = [
 
 export default function Sidebar(props) {
   const classes = useStyle();
+  const [selectedItem, setSelectedItem] = React.useState(null);
 
   return (
     <Drawer
@@ -100,12 +102,77 @@ export default function Sidebar(props) {
       >
         <div className={classes.toolbar} />
         <List>
-          {LIST_ITEMS.map(({name, label}, i) =>
-            <ListItem key={i} button component={Link} to={getRoute(name).path}>
-              <ListItemText primary={label} />
-            </ListItem>
+          {LIST_ITEMS.map((item, i) =>
+            <SidebarItem
+              key={i}
+              onSelect={setSelectedItem}
+              selectedName={selectedItem}
+              {...item}
+            />
           )}
         </List>
     </Drawer>
+  );
+}
+
+const useItemStyle = makeStyles(theme => ({
+  nested: {
+    paddingLeft: theme.spacing(2),
+  },
+}));
+
+function SidebarItem(props) {
+  const classes = useItemStyle();
+
+  const {label, name, subList=[], selectedName, onSelect, ...other} = props;
+  const linkPath = getRoute(name).path;
+  const urlMatch = useRouteMatch(linkPath);
+
+  function isSelected() {
+    return name == selectedName;
+  }
+
+  function shouldExpand() {
+    return isSelected() || !!subList.find(item => item.name == selectedName);
+  }
+
+  function setSelected() {
+    onSelect(name);
+  }
+
+  // Check if the item should be selected because of a direct access through the URL
+  if (urlMatch && urlMatch.isExact) {
+    if (!isSelected()) {
+      setSelected();
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <ListItem
+        button
+        component={Link}
+        selected={isSelected()}
+        onClick={setSelected}
+        to={linkPath}
+        {...other}
+      >
+        <ListItemText primary={label} />
+      </ListItem>
+      {subList.length > 0 &&
+        <Collapse in={shouldExpand()}>
+          <List component="div" disablePadding className={classes.nested}>
+            {subList.map((item, i) =>
+              <SidebarItem
+                key={i}
+                onSelect={onSelect}
+                selectedName={selectedName}
+                {...item}
+              />
+            )}
+          </List>
+        </Collapse>
+      }
+    </React.Fragment>
   );
 }
