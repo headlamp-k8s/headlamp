0f586359c3063d1711fb859e7a8eb2d135e27e7e
Joaquim Rocha <joaquim@kinvolk.io>
2019-12-05 15:29:07 +0100

    frontend: Add dedicated StatefulSet details view

---
+++ frontend/src/components/statefulset/Details.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import api, { useConnectApi } from '../../lib/api';
+import { MainInfoSection, MetadataDictGrid } from '../common/Resource';
+
+export default function StatefulSet() {
+  let { namespace, name } = useParams();
+  const [item, setItem] = React.useState(null);
+
+  useConnectApi(
+    api.statefulSet.get.bind(null, namespace, name, setItem),
+  );
+
+  return (
+    <MainInfoSection
+      resource={item}
+      extraInfo={item && [
+        {
+          name: 'Update Strategy',
+          value: item.spec.updateStrategy.type,
+        },
+        {
+          name: 'Selector',
+          value: <MetadataDictGrid dict={item.spec.selector.matchLabels} />,
+        },
+      ]}
+    />
+  );
+}
