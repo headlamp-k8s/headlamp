ff59d2d2819b066f33575875b1de703f23061415
Joaquim Rocha <joaquim@kinvolk.io>
2019-12-10 23:56:16 +0000

    frontend: Add filtering to the events section in the Cluster overview

--- frontend/src/components/cluster/Overview.js
+++ frontend/src/components/cluster/Overview.js
@@ -3,9 +3,10 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import React from 'react';
 import api, { useConnectApi } from '../../lib/api';
-import { timeAgo } from '../../lib/util';
+import { timeAgo, useFilterFunc } from '../../lib/util';
 import { makeStatusLabel } from '../common/Label';
 import { SectionBox } from '../common/SectionBox';
+import SectionFilterHeader from '../common/SectionFilterHeader';
 import SectionHeader from '../common/SectionHeader';
 import SimpleTable from '../common/SimpleTable';
 import { LightTooltip } from '../common/Tooltip';
@@ -70,10 +71,15 @@ export default function Overview(props) {
 
 function EventsSection(props) {
   const { events } = props;
+  const filterFunc = useFilterFunc();
 
   return (
     <Paper>
-      <SectionHeader title="Events" />
+      <SectionFilterHeader
+        title="Events"
+        // Not filtering since we don't show a namespace column in the events table
+        noNamespaceFilter
+      />
       <SectionBox>
         <SimpleTable
           rowsPerPage={[15, 25, 50]}
