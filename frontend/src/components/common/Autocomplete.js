33f6fd633fa425986fb7f6ae539501baeacf2cc8
Joaquim Rocha <joaquim@kinvolk.io>
2019-12-10 15:18:31 +0000

    frontend: Add SectionFilterHeader component
    This new component is the same as the SectionHeader but introduces two
    common actions related to filtering resources. Those are the search, and
    the namespaces filters.
    
    It uses also a new Autocomplete component for the namespaces filter,
    which is based in the Material-UI lab module, but this may change soon
    (because we want to get more control beyond what this module gives us,
    so this is just a quick solution).

---
+++ frontend/src/components/common/Autocomplete.js
@@ -0,0 +1,83 @@
+import checkBoxOutline from '@iconify/icons-mdi/check-box-outline';
+import checkboxBlankOutline from '@iconify/icons-mdi/checkbox-blank-outline';
+import { Icon } from '@iconify/react';
+import Box from '@material-ui/core/Box';
+import Checkbox from '@material-ui/core/Checkbox';
+import { useTheme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import api, { useConnectApi } from '../../lib/api';
+import { setNamespaceFilter } from '../../redux/actions/actions';
+
+// @todo: This is using Autcomplete from Material-UI's lab, but we should instead replace it
+// by a fully custom solution, as getting the input to behave the way we intended proved difficult
+// and not worth of pulling the extra dependencies.
+export function NamespacesAutocomplete(props) {
+  const theme = useTheme();
+  const filter = useSelector(state => state.filter);
+  const [namespaces, setNamespaces] = React.useState([]);
+
+  useConnectApi(
+    api.namespace.list.bind(null, setNamespaces),
+  );
+
+  const dispatch = useDispatch();
+
+  function renderTags(tags) {
+    let jointTags = tags.join(', ');
+    if (jointTags.length > 15) {
+      jointTags = jointTags.slice(0, 15) + '…';
+    }
+
+    return (
+      <Typography>{jointTags}</Typography>
+    );
+  }
+
+  return (
+    <Autocomplete
+      multiple
+      id="namespaces-filter"
+      autoComplete
+      options={namespaces.map(namespace => namespace.metadata.name)}
+      defaultValue={[]}
+      onChange={(event, newValue) => {
+        dispatch(setNamespaceFilter(newValue));
+        return [newValue];
+      }}
+      // We reverse the namespaces so the last chosen appear as the first in the label. This
+      // is useful since the label is ellipsized and this we get to see it change.
+      value={[...filter.namespaces.values()].reverse()}
+      renderOption={(option, { selected }) => (
+        <React.Fragment>
+          <Checkbox
+            icon={<Icon icon={checkboxBlankOutline} />}
+            checkedIcon={<Icon icon={checkBoxOutline} />}
+            style={{
+              color: selected ? theme.palette.primary.main : theme.palette.text.primary }}
+            checked={selected}
+          />
+          {option}
+        </React.Fragment>
+      )}
+      renderTags={renderTags}
+      renderInput={params => (
+        <Box width="15rem">
+          <TextField
+            {...params}
+            variant="standard"
+            label="Namespaces"
+            margin="normal"
+            fullWidth
+            InputLabelProps={{shrink: true}}
+            style={{marginTop: 0}}
+            placeholder={[...filter.namespaces.values()].length > 0 ? '' : 'Filter'}
+          />
+        </Box>
+      )}
+    />
+  );
+}
---
+++ frontend/src/components/common/SectionFilterHeader.js
@@ -0,0 +1,101 @@
+import filterVariantRemove from '@iconify/icons-mdi/filter-variant-remove';
+import { Icon } from '@iconify/react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetFilter, setSearchFilter } from '../../redux/actions/actions';
+import { NamespacesAutocomplete } from './Autocomplete';
+import SectionHeader from './SectionHeader';
+
+const useStyles = makeStyles(theme => ({
+  filterAction: {
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+export default function SectionFilterHeader(props) {
+  const { noNamespaceFilter=false, noSearch=false, ...headerProps } = props;
+  const classes = useStyles();
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  function resetFilters() {
+    dispatch(resetFilter());
+  }
+
+  React.useEffect(() => {
+    // We don't want the search to be used globally, but we're using Redux with it because
+    // this way we manage it the same way as with the other filters.
+    return function cleanup() {
+      dispatch(setSearchFilter(''));
+    };
+  },
+  []);
+
+  const hasNamespaceFilters = !noNamespaceFilter && [...filter.namespaces.values()].length > 0;
+  const hasSearch = !noSearch && filter.search;
+
+  const actions = [
+  ];
+
+  if (!noNamespaceFilter) {
+    actions.push(<NamespacesAutocomplete />);
+  }
+
+  actions.push(
+    <TextField
+      id="standard-search"
+      label="Search"
+      type="search"
+      InputLabelProps={{shrink: true}}
+      placeholder="Filter"
+      value={filter.search}
+      onChange={event =>
+        dispatch(setSearchFilter(event.target.value))
+      }
+    />
+  );
+
+  return (
+    <React.Fragment>
+      <SectionHeader
+        {...headerProps}
+        actions={actions.length <= 1 ? actions : [
+          <Grid
+            container
+            item
+            spacing={1}
+          >
+            {actions.map((action, i) =>
+              <Grid item key={i}>
+                {action}
+              </Grid>
+            )}
+          </Grid>
+        ]}
+      />
+      {(hasNamespaceFilters || hasSearch) &&
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          className={classes.filterAction}
+        >
+          <Grid item>
+            <Button
+              variant="outlined"
+              color="secondary"
+              endIcon={<Icon icon={filterVariantRemove} />  }
+              onClick={resetFilters}
+            >
+              Reset Filters
+            </Button>
+          </Grid>
+        </Grid>
+      }
+    </React.Fragment>
+  );
+}
