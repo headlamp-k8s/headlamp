fe3c9e0e49e3243022dbcfabbf8d54f9733a0cbf
Joaquim Rocha <joaquim@kinvolk.io>
2020-01-02 17:12:15 +0100

    frontend: Add redux-saga with a saga for deleting cluster objects
    This patch is introduces a sagas managing the cluster objects deletion.
    The actual connection to the UI will come in a later commit.

--- frontend/src/redux/stores/store.js
+++ frontend/src/redux/stores/store.js
@@ -1,13 +1,22 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { INITIAL_STATE as FILTER_INITIAL_STATE } from '../reducers/filter';
 import reducers from '../reducers/reducers';
+import rootSaga from '../sagas/sagas';
 
 const initialState = {
   filter: FILTER_INITIAL_STATE,
 };
 
-export default createStore(
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
   reducers,
   initialState,
+  applyMiddleware(sagaMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+export default store;
+
+sagaMiddleware.run(rootSaga);
