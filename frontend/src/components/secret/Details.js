5f596c81b1cb3a5504859a4f1f2cf9f7b80f90f6
Joaquim Rocha <joaquim@kinvolk.io>
2020-01-02 17:12:36 +0100

    frontend: Add a delete button to the secrets' details
    This delete button starts an object deletion action that will be done
    from the new related saga.

--- frontend/src/components/secret/Details.js
+++ frontend/src/components/secret/Details.js
@@ -1,16 +1,29 @@
+import deleteIcon from '@iconify/icons-mdi/delete';
+import { Icon } from '@iconify/react';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 import _ from 'lodash';
 import React from 'react';
-import { useParams } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { useLocation, useParams } from "react-router-dom";
 import api, { useConnectApi } from '../../lib/api';
+import { getRoute } from '../../lib/router';
+import { deleteClusterObjects } from '../../redux/actions/actions';
 import { MainInfoSection, SecretField } from '../common/Resource';
 
 export default function SecretDetails(props) {
   let { namespace, name } = useParams();
   const [item, setItem] = React.useState(null);
+  const dispatch = useDispatch();
+  const location = useLocation();
 
   useConnectApi(
     api.secret.get.bind(null, namespace, name, setItem),
   );
+
+  function handleDelete() {
+    api.secret.delete(namespace, name);
+  }
 
   return (
     <MainInfoSection
@@ -21,6 +34,19 @@ export default function SecretDetails(props) {
           valueComponent: <SecretField label={key} value={value} />
         }
       ))}
+      actions={item && [
+        <Tooltip title="Delete">
+          <IconButton
+            aria-label="delete"
+            onClick={() => {
+              dispatch(deleteClusterObjects([item], handleDelete,
+                {startUrl: getRoute('secrets').path, cancelUrl: location.pathname }));
+            }}
+          >
+            <Icon icon={deleteIcon} />
+          </IconButton>
+        </Tooltip>,
+      ]}
     />
   );
 }
