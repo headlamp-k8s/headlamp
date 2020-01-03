358a1e7ae56c429dabe7683e737f4066e14700de
Joaquim Rocha <joaquim@kinvolk.io>
2020-01-03 15:07:43 +0100

    frontend: Add deletion to BindingDetails view

--- frontend/src/components/role/BindingDetails.js
+++ frontend/src/components/role/BindingDetails.js
@@ -2,6 +2,8 @@ import Paper from '@material-ui/core/Paper';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import api, { useConnectApi } from '../../lib/api';
+import { getRoute } from '../../lib/router';
+import DeleteButton from '../common/DeleteButton';
 import Loader from '../common/Loader';
 import { MainInfoSection, PageGrid } from '../common/Resource';
 import { SectionBox } from '../common/SectionBox';
@@ -19,6 +21,14 @@ export default function RoleBindingDetails() {
       api.clusterRoleBinding.get.bind(null, name, setItem)
     )
   );
+
+  function handleDelete() {
+    if (namespace) {
+      api.roleBinding.delete(namespace, name);
+    } else {
+      api.clusterRoleBinding.delete(name);
+    }
+  }
 
   return (
     !item ? <Loader /> :
@@ -39,6 +49,13 @@ export default function RoleBindingDetails() {
               name: 'Ref. API Group',
               value: item.roleRef.apiGroup,
             }
+          ]}
+          actions={item && [
+            <DeleteButton
+              items={[item]}
+              deletionCallback={handleDelete}
+              options={{startUrl: getRoute('roleBindings').path}}
+            />
           ]}
         />,
         <Paper>
