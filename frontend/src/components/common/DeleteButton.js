96375cd7238bc7501db9866b665ba30ceb07be5e
Joaquim Rocha <joaquim@kinvolk.io>
2020-02-06 15:06:12 +0100

    frontend: Verify the needed permissions in Edit/Delete buttons
    The buttons were being added to the sections regardless of whether the
    user was actually authorized to perform their respective action.
    
    These changes implement the authorization check in each button, only
    adding them to the UI in case the user has the needed permissions.

--- frontend/src/components/common/DeleteButton.js
+++ frontend/src/components/common/DeleteButton.js
@@ -5,6 +5,7 @@ import Tooltip from '@material-ui/core/Tooltip';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation } from "react-router-dom";
+import api from '../../lib/api';
 import { clusterAction } from '../../redux/actions/actions';
 import { ConfirmDialog } from './Dialog';
 
@@ -12,9 +13,23 @@ export default function DeleteButton(props) {
   const dispatch = useDispatch();
   const { item, deletionCallback, options } = props;
   const [openAlert, setOpenAlert] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
   const location = useLocation();
 
-  return (
+  React.useEffect(() => {
+    if (item && item.metadata) {
+      api.getAuthorization(item, 'delete').then(
+        result => {
+          if (result.status.allowed) {
+            setVisible(true);
+          }
+        }
+      );
+    }
+  },
+  [item]);
+
+  return visible && (
     <React.Fragment>
       <Tooltip title="Delete">
         <IconButton
