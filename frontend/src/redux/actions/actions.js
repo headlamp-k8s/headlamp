4617ca92f91575d831fd85629f188a11cfa483b7
Joaquim Rocha <joaquim@kinvolk.io>
2020-02-07 10:34:26 +0100

    frontend: Add the notion of UI/sidebar to redux
    These changes add the state, reducers, and actions for dealing with
    UI states (particularly the sidebar at this moment).
    
    The actual use will come in later commits.

--- frontend/src/redux/actions/actions.js
+++ frontend/src/redux/actions/actions.js
@@ -4,6 +4,8 @@ export const FILTER_SET_SEARCH = 'FILTER_SET_SEARCH';
 export const CLUSTER_ACTION = 'CLUSTER_ACTION';
 export const CLUSTER_ACTION_UPDATE = 'CLUSTER_ACTION_UPDATE';
 export const CLUSTER_ACTION_CANCEL = 'CLUSTER_ACTION_CANCEL';
+export const UI_SIDEBAR_SET_SELECTED = 'UI_SIDEBAR_SET_SELECTED';
+export const UI_SIDEBAR_SET_VISIBLE = 'UI_SIDEBAR_SET_VISIBLE';
 
 export function setNamespaceFilter(namespaces) {
   return { type: FILTER_SET_NAMESPACE, namespaces: namespaces };
@@ -23,4 +25,12 @@ export function clusterAction(actionCallback, actionOptions={}) {
 
 export function updateClusterAction(actionOptions) {
   return { type: CLUSTER_ACTION_UPDATE, ...actionOptions};
+}
+
+export function setSidebarSelected(selected) {
+  return { type: UI_SIDEBAR_SET_SELECTED, selected };
+}
+
+export function setSidebarVisible(isVisible) {
+  return { type: UI_SIDEBAR_SET_VISIBLE, isVisible };
 }
---
+++ frontend/src/redux/reducers/ui.js
@@ -0,0 +1,35 @@
+import { UI_SIDEBAR_SET_SELECTED, UI_SIDEBAR_SET_VISIBLE } from '../actions/actions';
+
+export const INITIAL_STATE = {
+  sidebar: {
+    selected: 'cluster',
+    isVisible: true,
+  }
+};
+
+function reducer(state = INITIAL_STATE, action) {
+  const newFilters = {...state};
+  switch(action.type) {
+    case UI_SIDEBAR_SET_SELECTED: {
+      newFilters.sidebar = {
+        ...newFilters.sidebar,
+        selected: action.selected,
+        isVisible: !!action.selected,
+      };
+      break;
+    }
+    case UI_SIDEBAR_SET_VISIBLE: {
+      newFilters.sidebar = {
+        ...newFilters.sidebar,
+        isVisible: action.isVisible,
+      };
+      break;
+    }
+    default:
+      return state;
+  };
+
+  return newFilters;
+}
+
+export default reducer;
