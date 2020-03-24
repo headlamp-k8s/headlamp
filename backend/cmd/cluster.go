19f9f6f16526b05c28e39108596101bc61252139
Joaquim Rocha <joaquim@kinvolk.io>
2020-03-24 17:30:32 +0100

    backend: Add multi-cluster support to the backend
    These changes allow the backend to serve several clusters instead of
    just one, and rearrange the code in several files so things are bit
    better abstracted.
    
    It still serves one cluster from the previously used endpoint
    (/cluster/{api...}) as this is needed until multi-cluster support is
    added to the frontend.

---
+++ backend/cmd/cluster.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+// @todo: Use a different way to avoid name clashes with other clusters
+const OWN_CLUSTER_NAME = "_main_"
+
+type Cluster struct {
+	name   string
+	config *clientcmdapi.Cluster
+}
+
+func GetClustersFromKubeConfigFile(kubeConfigPath string) ([]Cluster, error) {
+	config, err := clientcmd.LoadFromFile(kubeConfigPath)
+	if err != nil {
+		return nil, err
+	}
+
+	clusters := []Cluster{}
+
+	for key, value := range config.Clusters {
+		if value.Server == "" {
+			log.Fatalf("Please make sure all configured clusters have a URL!")
+		}
+		// @todo: Create a name from the URL instead of failing here
+		if key == "" {
+			log.Fatalf("Please make sure the cluster with URL %v has a name configured!", value.Server)
+		}
+		clusters = append(clusters, Cluster{key, value})
+	}
+
+	return clusters, nil
+}
+
+func GetOwnCluster() (*Cluster, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	cluster := &clientcmdapi.Cluster{
+		Server:                   config.Host,
+		CertificateAuthority:     config.TLSClientConfig.CAFile,
+		CertificateAuthorityData: config.TLSClientConfig.CAData,
+	}
+
+	return &Cluster{name: OWN_CLUSTER_NAME, config: cluster}, nil
+}
+
+func (c *Cluster) getServer() *string {
+	return &c.config.Server
+}
+
+func (c *Cluster) getName() *string {
+	return &c.name
+}
+
+func (c *Cluster) getCAData() []byte {
+	if c.config.CertificateAuthority != "" {
+		if pemBytes, err := ioutil.ReadFile(c.config.CertificateAuthority); err == nil {
+			return pemBytes
+		} else {
+			log.Fatal("Failed to add certificate:", err)
+		}
+	}
+
+	if caData := c.config.CertificateAuthorityData; len(caData) > 0 {
+		return caData
+	}
+
+	return nil
+}
+
+func (c *Cluster) shouldVerifyTLS() bool {
+	return !c.config.InsecureSkipTLSVerify
+}
--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -1,145 +1,25 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
-	"path/filepath"
-
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-
-	"crypto/tls"
-	"crypto/x509"
 	"flag"
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	var kubeconfig *string
-	if configFile := getKubeConfigFile(); configFile != "" {
-		kubeconfig = flag.String("kubeconfig", configFile, "(Optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "Absolute path to the kubeconfig file")
-	}
-
+	kubeconfig := flag.String("kubeconfig", "", "Absolute path to the kubeconfig file")
 	inCluster := flag.Bool("in-cluster", false, "Set when running from a k8s cluster")
-
 	devMode := flag.Bool("dev", false, "Allow connections from other origins")
-
+	staticDir := flag.String("html-static-dir", "", "Static HTML directory to serve")
+	insecure := flag.Bool("insecure-ssl", false, "Accept/Ignore all server SSL certificates")
 	// @todo: Make this a uint and validate the values
 	port := flag.String("port", "4654", "Port to listen from")
 
-	staticDir := flag.String("html-static-dir", "", "Static HTML directory to serve")
-
 	flag.Parse()
 
-	var k8sConfig *rest.Config = nil
-	if *inCluster {
-		var err error = nil
-		k8sConfig, err = rest.InClusterConfig()
-		if err != nil {
-			panic(err.Error())
-		}
-	}
-
-	// Use the current context in kubeconfig
-	if k8sConfig == nil {
-		var err error = nil
-		k8sConfig, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	// Get the server URL
-	serverURL := k8sConfig.Host
-	remote, err := url.Parse(serverURL)
-	if err != nil {
-		panic(err)
-	}
-
-	insecure := flag.Bool("insecure-ssl", false, "Accept/Ignore all server SSL certificates")
-	flag.Parse()
-
-	// Set up certificates for TLS
-	rootCAs := x509.NewCertPool()
-
-	if k8sConfig.TLSClientConfig.CAFile != "" {
-		if pemBytes, err := ioutil.ReadFile(k8sConfig.TLSClientConfig.CAFile); err == nil {
-			ok := rootCAs.AppendCertsFromPEM(pemBytes)
-			if !ok {
-				log.Println("Failed to add certificate")
-			}
-		}
-	}
-
-	if certificates := k8sConfig.TLSClientConfig.CAData; len(certificates) > 0 {
-		if rootCAs.AppendCertsFromPEM(certificates) {
-			log.Println("No certificates were found")
-		}
-	}
-
-	config := &tls.Config{
-		InsecureSkipVerify: *insecure,
-		RootCAs:            rootCAs,
-	}
-	tr := &http.Transport{TLSClientConfig: config}
-
-	// Create a reverse proxy to direct the API calls to the right server
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-	proxy.Transport = tr
-
-	r := mux.NewRouter()
-	r.HandleFunc("/cluster/{api:.*}", handler(remote, proxy))
-
-	// Serve the frontend if needed
-	if *staticDir != "" {
-		fs := http.FileServer(http.Dir(*staticDir))
-		r.PathPrefix("/").Handler(fs)
-	}
-
-	http.Handle("/", r)
-
-	var handler http.Handler
-
-	// On dev mode we're loose about w
-	if *devMode {
-		headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
-		methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "DELETE", "OPTIONS"})
-		origins := handlers.AllowedOrigins([]string{"*"})
-		handler = handlers.CORS(headers, methods, origins)(r)
-	} else {
-		handler = r
-	}
-
-	log.Println("Lokodash Server:")
-	log.Println("API Router:")
-	log.Println("\t", "localhost:"+*port+"/cluster/{api...} ->", serverURL)
-
-	// Start server
-	log.Fatal(http.ListenAndServe(":"+*port, handler))
-}
-
-func handler(url *url.URL, proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		request.Host = url.Host
-		request.Header.Set("X-Forwarded-Host", request.Header.Get("Host"))
-		request.URL.Host = url.Host
-		request.URL.Path = mux.Vars(request)["api"]
-		request.URL.Scheme = url.Scheme
-
-		log.Println("Requesting ", request.URL.Scheme, request.URL.Host, mux.Vars(request)["api"])
-		proxy.ServeHTTP(writer, request)
-	}
-}
-
-func getKubeConfigFile() string {
-	return filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	StartHeadlampServer(&HeadlampConfig{
+		useInCluster:   *inCluster,
+		kubeConfigPath: *kubeconfig,
+		port:           *port,
+		devMode:        *devMode,
+		staticDir:      *staticDir,
+		insecure:       *insecure})
 }
