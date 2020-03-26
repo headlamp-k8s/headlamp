28680f384b23d8d2a2968df37ebe23cd2ad11dfd
Joaquim Rocha <joaquim@kinvolk.io>
2020-03-26 21:40:31 +0100

    frontend: Remove unused methods from lib/auth
    These were onTokenChange + addHandler. The goal is simply to make the
    file simpler for the coming changes (and remove useless code anyway).

--- frontend/src/lib/auth.js
+++ frontend/src/lib/auth.js
@@ -2,10 +2,7 @@
  * This module was taken from the k8dash project.
  */
 
-import _ from 'lodash';
 import * as cookie from 'js-cookie';
-
-const handlers = [];
 
 // If we have an "Authorization" cookie, use that as the token for future api calls
 const authorizationCookie = cookie.get('Authorization');
@@ -29,26 +26,13 @@ export function hasToken() {
 
 export function setToken(token) {
     localStorage.authToken = token;
-    onTokenChange();
 }
 
 export function deleteToken() {
     delete localStorage.authToken;
-    onTokenChange();
 }
 
 export function logout() {
     deleteToken();
     //window.location.reload();
-}
-
-export function addHandler(handler) {
-    handlers.push(handler);
-    return () => {
-        _.pull(handlers, handler);
-    };
-}
-
-function onTokenChange() {
-    handlers.forEach(x => x());
 }
