990032b9a4c2f4198f041aa417a22a8648106781
Joaquim Rocha <joaquim@kinvolk.io>
2020-04-23 17:40:20 +0200

    frontend: Remove react-app-rewired
    react-app-rewired was added before to add configuration related to the
    monaco-editor. But the configuration doesn't seem to be necessary now,
    and react-app-rewired is deprecated anyway.

--- frontend/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-module.exports = {
-  plugins: [
-    new MonacoWebpackPlugin({
-      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-      languages: ['yaml']
-    })
-  ]
-};
