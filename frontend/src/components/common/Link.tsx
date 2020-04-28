1f0887d1b8ef9e4b05d976803c7d5b4d3d3c9e05
Joaquim Rocha <joaquim@kinvolk.io>
2020-04-28 12:06:05 +0200

    frontend: Port common/Link component to Typescript

--- frontend/src/components/common/Link.tsx
+++ frontend/src/components/common/Link.tsx
@@ -1,9 +1,14 @@
 import MuiLink from '@material-ui/core/Link';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { createRouteURL } from '../../lib/router';
+import { createRouteURL, RouteURLProps } from '../../lib/router';
 
-export default function Link(props) {
+interface LinkProps {
+  routeName: string;
+  params?: RouteURLProps;
+}
+
+export default function Link(props: React.PropsWithChildren<LinkProps>) {
   const { routeName, params = {} } = props;
   return (
     <MuiLink component={RouterLink} to={createRouteURL(routeName, params)}>
