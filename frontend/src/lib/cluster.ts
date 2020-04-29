a8037e12d3ab0d7161597f82c9bf08a188ede96b
Joaquim Rocha <joaquim@kinvolk.io>
2020-04-29 14:03:31 +0200

    frontend: Add cluster module with cluster object related interfaces
    This patch introduces the mentioned module so we can start using the
    common objects' types/interfaces where needed.

---
+++ frontend/src/lib/cluster.ts
@@ -0,0 +1,104 @@
+export interface KubeObject {
+  kind: string;
+  apiVersion?: string;
+  metadata: KubeMetadata;
+  [otherProps: string]: any;
+}
+
+export interface StringDict {
+  [key: string]: string;
+}
+
+export interface KubeMetadata {
+  uid: string;
+  name: string;
+  namespace?: string;
+  creationTimestamp: string;
+  resourceVersion: string;
+  selfLink: string;
+  labels?: StringDict;
+  annotations?: StringDict;
+}
+
+export interface KubeCondition {
+  type: string;
+  status: string;
+  lastProbeTime: number;
+  lastTransitionTime?: string;
+  lastUpdateTime?: string;
+  reason?: string;
+  message?: string;
+}
+
+export interface KubeContainer {
+  name: string;
+  image: string;
+  command?: string[];
+  args?: string[];
+  ports: {
+    name?: string;
+    containerPort: number;
+    protocol: string;
+  }[];
+  resources?: {
+    limits: {
+      cpu: string;
+      memory: string;
+    };
+    requests: {
+      cpu: string;
+      memory: string;
+    };
+  };
+  env?: {
+    name: string;
+    value?: string;
+    valueFrom?: {
+      fieldRef?: {
+        apiVersion: string;
+        fieldPath: string;
+      };
+      secretKeyRef?: {
+        key: string;
+        name: string;
+      };
+      configMapKeyRef?: {
+        key: string;
+        name: string;
+      };
+    };
+  }[];
+  envFrom?: {
+    configMapRef?: {
+      name: string;
+    };
+  }[];
+  volumeMounts?: {
+    name: string;
+    readOnly: boolean;
+    mountPath: string;
+  }[];
+  livenessProbe?: KubeContainerProbe;
+  readinessProbe?: KubeContainerProbe;
+  imagePullPolicy: string;
+}
+
+interface KubeContainerProbe {
+  httpGet?: {
+    path?: string;
+    port: number;
+    scheme: string;
+    host?: string;
+  };
+  exec?: {
+    command: string[];
+  };
+  tcpSocket?: {
+    port: number;
+  };
+  initialDelaySeconds?: number;
+  timeoutSeconds?: number;
+  periodSeconds?: number;
+  successThreshold?: number;
+  failureThreshold?: number;
+}
