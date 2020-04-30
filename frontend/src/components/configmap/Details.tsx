5c7736a263a507b174dd64d7a04ab024f6c0aa22
Joaquim Rocha <joaquim@kinvolk.io>
2020-04-30 16:00:09 +0200

    frontend: Port configmap components to Typescript
    Besides the port, this patch also adds a new KubeConfigMap type so we
    can specialize this type of object.

--- frontend/src/components/configmap/Details.tsx
+++ frontend/src/components/configmap/Details.tsx
@@ -2,15 +2,17 @@ import Paper from '@material-ui/core/Paper';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import api, { useConnectApi } from '../../lib/api';
+import { KubeConfigMap } from '../../lib/cluster';
 import Empty from '../common/EmptyContent';
 import Loader from '../common/Loader';
 import { DataField, MainInfoSection, PageGrid } from '../common/Resource';
 import { SectionBox } from '../common/SectionBox';
 import SectionHeader from '../common/SectionHeader';
 
-export default function ConfigDetails(props) {
+export default function ConfigDetails() {
   const { namespace, name } = useParams();
-  const [item, setItem] = React.useState(null);
+  const [item, setItem] = React.useState<KubeConfigMap | null>(null);
+  const itemData = item?.data;
 
   useConnectApi(
     api.configMap.get.bind(null, namespace, name, setItem),
@@ -23,12 +25,12 @@ export default function ConfigDetails(props) {
         <MainInfoSection resource={item} />,
         <Paper>
           <SectionHeader title="Data" />
-          {!item.data ?
+          {!itemData ?
             <Empty>No data in this config map</Empty>
-            : Object.keys(item.data).map((key, i) =>
+            : Object.keys(itemData).map((key, i) =>
               <React.Fragment key={i}>
                 <SectionBox marginBottom="2rem">
-                  <DataField label={key} value={item.data[key]} />
+                  <DataField label={key} value={itemData[key]} />
                 </SectionBox>
               </React.Fragment>
             )
--- frontend/src/lib/cluster.ts
+++ frontend/src/lib/cluster.ts
@@ -119,3 +119,7 @@ export interface KubeEvent {
   };
   [otherProps: string]: any;
 }
+
+export interface KubeConfigMap extends KubeObject {
+  data: StringDict;
+}
