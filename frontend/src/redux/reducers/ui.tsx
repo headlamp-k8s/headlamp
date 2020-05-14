c7e403d64d06b9c7d7b6fdc942bf93994ce650ef
Joaquim Rocha <joaquim@kinvolk.io>
2020-05-14 15:45:04 +0200

    frontend: Specify a type for header actions
    We save header actions (components in the header section of views) in
    the reducers/ui's state, and we should have a specific type assigned to
    them.

--- frontend/src/redux/reducers/ui.tsx
+++ frontend/src/redux/reducers/ui.tsx
@@ -23,7 +23,7 @@ export interface UIState {
   views: {
     details: {
       headerActions: {
-        [name: string]: any;
+        [name: string]: (...args: any[]) => React.ReactNode | null;
       };
     };
   };
