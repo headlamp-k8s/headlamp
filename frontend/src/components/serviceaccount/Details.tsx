bbd72ec3788deec1de6993d38c4d5c9108feaa5c
Joaquim Rocha <joaquim@kinvolk.io>
2020-05-15 09:34:42 +0200

    frontend: Port serviceaccount components to Typescript

--- frontend/src/components/serviceaccount/Details.tsx
+++ frontend/src/components/serviceaccount/Details.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import api, { useConnectApi } from '../../lib/api';
+import { KubeSecretAccount } from '../../lib/cluster';
 import { getRoute } from '../../lib/router';
 import DeleteButton from '../common/DeleteButton';
 import EditButton from '../common/EditButton';
 import { MainInfoSection } from '../common/Resource';
 
-export default function ServiceAccountDetails(props) {
+export default function ServiceAccountDetails() {
   const { namespace, name } = useParams();
-  const [item, setItem] = React.useState(null);
+  const [item, setItem] = React.useState<KubeSecretAccount | null>(null);
 
   console.log(item);
 
@@ -16,7 +17,7 @@ export default function ServiceAccountDetails(props) {
     api.serviceAccount.get.bind(null, namespace, name, setItem),
   );
 
-  function handleApply(newItem) {
+  function handleApply(newItem: KubeSecretAccount) {
     api.serviceAccount.put(newItem);
   }
 
