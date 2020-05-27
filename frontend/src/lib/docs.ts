35c51fd37d1ad24381552520f308af7bf2c73da6
Joaquim Rocha <joaquim@kinvolk.io>
2020-05-27 16:38:34 +0200

    frontend: Port lib/docs to Typescript

--- frontend/src/lib/docs.ts
+++ frontend/src/lib/docs.ts
@@ -2,22 +2,23 @@
  * This module was taken from the k8dash project.
  */
 
+import { OpenAPIV2 } from 'openapi-types';
 import Swagger from 'swagger-parser';
 import apis from './api';
 
-let docsPromise;
+let docsPromise: ReturnType<typeof getDocs>;
 
 async function getDocs() {
   const docs = await apis.swagger();
   return Swagger.dereference(docs);
 }
 
-export default async function getDocDefinitions(apiVersion, kind) {
+export default async function getDocDefinitions(apiVersion: string, kind: string) {
   if (!docsPromise) {
     docsPromise = getDocs(); // Don't wait here. Just kick off the request
   }
 
-  const {definitions} = await docsPromise;
+  const {definitions = {}} = (await docsPromise) as OpenAPIV2.Document;
 
   let [group, version] = apiVersion.split('/');
   if (!version) {
@@ -29,7 +30,7 @@ export default async function getDocDefinitions(apiVersion, kind) {
     .filter(x => !!x['x-kubernetes-group-version-kind'])
     .find(x => x['x-kubernetes-group-version-kind'].some(comparer));
 
-  function comparer(info) {
+  function comparer(info: OpenAPIV2.SchemaObject) {
     return info.group === group
             && info.version === version
             && info.kind === kind;
