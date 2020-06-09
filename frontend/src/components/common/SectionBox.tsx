27c24089a4d004097b1cdb44ff96cb41670e468d
Joaquim Rocha <joaquim@kinvolk.io>
2020-06-09 10:59:41 +0200

    frontend: Add a title property to the SectionBox
    Most of the SectionBoxes are used together with a SectionHeader
    component with a title in it. So these changes aim to reduce the
    repeated code by allowing a "title" property in the SectionBox
    directly: if the title is a string, it'll be used in a SectionHeader,
    if it is another type of React node, then it'll be used directly.

--- frontend/src/components/common/SectionBox.tsx
+++ frontend/src/components/common/SectionBox.tsx
@@ -1,8 +1,30 @@
 import Box, { BoxProps } from '@material-ui/core/Box';
 import React from 'react';
+import SectionHeader from './SectionHeader';
 
-export function SectionBox(props: BoxProps) {
+interface SectionBoxProps extends Omit<BoxProps, 'title'> {
+  title?: React.ReactNode;
+}
+
+export function SectionBox(props: SectionBoxProps) {
+  const {title, children, ...otherProps} = props;
+
+  let titleElem: React.ReactNode;
+
+  if (typeof title === 'string') {
+    titleElem = <SectionHeader noPadding title={title as string} />;
+  } else {
+    titleElem = title as JSX.Element;
+  }
+
   return (
-    <Box px={2} paddingBottom={1} {...props} />
+    <Box
+      px={2}
+      paddingBottom={1}
+      {...otherProps}
+    >
+      { title && titleElem }
+      {React.Children.toArray(children)}
+    </Box>
   );
 }
