4346066cc1157a7d7d3e0f9f041e33717670f937
Joaquim Rocha <joaquim@kinvolk.io>
2020-06-10 15:43:11 +0200

    frontend: Allow a headerProps property in the SectionBox
    Now that we allow to set different styles in the header, we need to pass
    the desired style also when using the header from within the SectionBox.

--- frontend/src/components/common/SectionBox.tsx
+++ frontend/src/components/common/SectionBox.tsx
@@ -1,18 +1,28 @@
 import Box, { BoxProps } from '@material-ui/core/Box';
 import React from 'react';
-import SectionHeader from './SectionHeader';
+import SectionHeader, { SectionHeaderProps } from './SectionHeader';
 
 interface SectionBoxProps extends Omit<BoxProps, 'title'> {
   title?: React.ReactNode;
+  headerProps?: Omit<SectionHeaderProps, 'title'>;
 }
 
 export function SectionBox(props: SectionBoxProps) {
-  const {title, children, ...otherProps} = props;
+  const {
+    title,
+    children,
+    headerProps = {noPadding: false, headerStyle: 'subsection'},
+    ...otherProps
+  } = props;
 
   let titleElem: React.ReactNode;
 
   if (typeof title === 'string') {
-    titleElem = <SectionHeader noPadding title={title as string} />;
+    titleElem =
+      <SectionHeader
+        title={title as string}
+        {...headerProps}
+      />;
   } else {
     titleElem = title as JSX.Element;
   }
