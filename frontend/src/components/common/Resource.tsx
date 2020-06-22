4f22754d9d8b77bf9831c38f921b98f672745883
Joaquim Rocha <joaquim@kinvolk.io>
2020-06-22 12:04:26 +0200

    frontend: Use the right type for the MainInfoSection's resource
    The resource parameter was being used as a KubeObjectInterface type, but
    actually an instance of KubeObject is being passed by every user of the
    component. So this patch sets the right type to the mentioned property.

--- frontend/src/components/common/Resource.tsx
+++ frontend/src/components/common/Resource.tsx
@@ -13,7 +13,7 @@ import TextField, { TextFieldProps } from '@material-ui/core/TextField';
 import Typography, { TypographyProps } from '@material-ui/core/Typography';
 import _ from 'lodash';
 import React from 'react';
-import { KubeCondition, KubeContainer, KubeObjectInterface } from '../../lib/k8s/cluster';
+import { KubeCondition, KubeContainer, KubeObject, KubeObjectInterface } from '../../lib/k8s/cluster';
 import { RouteURLProps } from '../../lib/router';
 import { localeDate } from '../../lib/util';
 import { useTypedSelector } from '../../redux/reducers/reducers';
@@ -197,7 +197,7 @@ export function ResourceLink(props: ResourceLinkProps) {
 }
 
 interface MainInfoSectionProps {
-  resource: KubeObjectInterface | null;
+  resource: KubeObject | null;
   headerSection?: React.ReactNode;
   title?: string;
   extraInfo?: NameValueTableRow[] | null;
