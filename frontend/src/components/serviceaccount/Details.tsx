081cf3334023449e1ebc692fa6fe9331c42255b0
Joaquim Rocha <joaquim@kinvolk.io>
2020-06-22 17:29:02 +0200

    frontend: Remove direct EditButton use from serviceaccount/Details
    The mentioned component was using an EditButton and setting up the
    handler function for it. We want users to be able to edit any objects
    (as long as they have permissions) so we will add a more automated way
    of providing this functionality. Thus, this patch removes the use of the
    EditButton from the ServiceAccount's details view.

--- frontend/src/components/serviceaccount/Details.tsx
+++ frontend/src/components/serviceaccount/Details.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import api from '../../lib/k8s/api';
 import ServiceAccount from '../../lib/k8s/serviceAccount';
-import EditButton from '../common/EditButton';
 import { MainInfoSection } from '../common/Resource';
 
 export default function ServiceAccountDetails() {
@@ -10,10 +8,6 @@ export default function ServiceAccountDetails() {
   const [item, setItem] = React.useState<ServiceAccount | null>(null);
 
   ServiceAccount.useApiGet(setItem, name, namespace);
-
-  function handleApply(newItem: ServiceAccount) {
-    api.serviceAccount.put(newItem);
-  }
 
   return (
     <MainInfoSection
@@ -23,15 +17,6 @@ export default function ServiceAccountDetails() {
           name: 'Secrets',
           value: item.secrets.map(({name}) => name).join(', ')
         }
-      ]}
-      actions={item && [
-        <EditButton
-          item={item}
-          applyCallback={handleApply}
-          options={{
-            successOptions: {variant: 'success'}
-          }}
-        />
       ]}
     />
   );
