0044acd14ea38bbcdb96dad2715330f145ac8d36
Joaquim Rocha <joaquim@kinvolk.io>
2020-07-13 13:07:14 +0200

    backend: Fix serving static files for unregistered routes
    The backend was only redirecting the root route to the static server and
    this meant that if a user accessed a frontend URL that was not the root
    directly, a 404 would be returned.
    
    In order to fix the issue, this patch adds a new Single Page Application
    handler (taken from mux's docs) that either serves files directly (when
    they exist) or redirects the routes to the frontend's handler.

--- backend/cmd/headlamp.go
+++ backend/cmd/headlamp.go
@@ -30,6 +30,41 @@ type clientConfig struct {
 	Clusters []Cluster `json:"clusters"`
 }
 
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func StartHeadlampServer(config *HeadlampConfig) {
 	kubeConfigPath := ""
 
@@ -78,10 +113,8 @@ func StartHeadlampServer(config *HeadlampConfig) {
 	r.HandleFunc("/config", clientConf.getConfig).Methods("GET")
 
 	// Serve the frontend if needed
-	if config.staticDir != "" {
-		fs := http.FileServer(http.Dir(config.staticDir))
-		r.PathPrefix("/").Handler(fs)
-	}
+	spa := spaHandler{staticPath: config.staticDir, indexPath: "index.html"}
+	r.PathPrefix("/").Handler(spa)
 
 	http.Handle("/", r)
 
