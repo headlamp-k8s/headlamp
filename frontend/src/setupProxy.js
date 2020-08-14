7fea12cae440c815cdf3d453acdfcba05f10a0d4
Ashu Ghildiyal <ashu@kinvolk.io>
2020-08-14 23:10:35 +0530

    frontend: Proxy /plugins request to backend
    We want to proxy /plugins request to the backend. This patch adds
    http-proxy-middleware which allows us proxy requests from the frontend
    server.

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use(
+    '/plugins',
+    createProxyMiddleware({
+      target: 'http://localhost:4654',
+      changeOrigin: true,
+    })
+  );
+};
