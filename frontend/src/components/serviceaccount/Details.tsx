829dcb9944084cf60b39754c3bb2c8599b2a08da
ashu ghildiyal <ashu@kinvolk.io>
2020-12-03 15:10:21 +0530

    frontend: Add link to secret view from service account
    If we are in the service account and are looking a secret name than we
    right now have to copy it and than look it up in the secrets list which
    is a hectic work instead we should directly connect the secret details
    page from the service accounts details page.

--- frontend/src/components/serviceaccount/Details.tsx
+++ frontend/src/components/serviceaccount/Details.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ServiceAccount from '../../lib/k8s/serviceAccount';
+import { Link } from '../common';
 import { MainInfoSection } from '../common/Resource';
 
 export default function ServiceAccountDetails() {
@@ -15,7 +16,18 @@ export default function ServiceAccountDetails() {
       extraInfo={item && [
         {
           name: 'Secrets',
-          value: item.secrets.map(({name}) => name).join(', ')
+          value: <React.Fragment>
+            {
+              item.secrets.map(({name}, index) =>
+                <React.Fragment key={`${name}__${index}`}>
+                  <Link routeName={'secret'} params={{namespace, name}}>
+                    {name}
+                  </Link>
+                  {index !== item.secrets.length - 1 && ','}
+                </React.Fragment>
+              )
+            }
+          </React.Fragment>
         }
       ]}
     />
