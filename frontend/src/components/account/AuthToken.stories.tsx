c6fa8aec87583ec9298c358aa4d1a691198276c8
René Dudfield <rene@kinvolk.io>
2021-01-11 18:56:02 +0100

    frontend: add src/components/account/Auth stories.

---
+++ frontend/src/components/account/AuthToken.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+import { PureAuthToken, PureAuthTokenProps } from './Auth';
+
+export default {
+  title: 'AuthToken',
+  component: PureAuthToken,
+  argTypes: {
+    onCancel: { action: 'cancel clicked' },
+    onAuthClicked: { action: 'auth clicked' },
+    onChangeToken: { action: 'token changed' },
+    onCloseError: { action: 'error closed' },
+  },
+} as Meta;
+
+const Template: Story<PureAuthTokenProps> = (args) => (
+  <PureAuthToken {...args} />
+);
+
+export const ShowError = Template.bind({});
+ShowError.args = {
+  title: 'a title',
+  token: 'a token',
+  showError: true,
+  showActions: false,
+};
+
+export const ShowActions = Template.bind({});
+ShowActions.args = {
+  title: 'a title',
+  token: 'a token',
+  showError: false,
+  showActions: true,
+};
