60cffe64e2e225016c03c8824399bb9f46055586
René Dudfield <rene@kinvolk.io>
2021-01-11 18:53:28 +0100

    frontend: add src/components/common/Tab stories

---
+++ frontend/src/components/common/Tabs.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+import Tabs, { TabsProps } from './Tab';
+
+export default {
+  title: 'Tabs',
+  component: Tabs,
+  argTypes: { onTabChanged: { action: 'tab changed' } },
+} as Meta;
+
+const Template: Story<TabsProps> = (args) => <Tabs {...args} />;
+
+export const BasicTabs = Template.bind({});
+BasicTabs.args = {
+  tabs: [
+    {
+      label: 'tab 1 label',
+      component: <p>tab body 1</p>,
+    },
+    {
+      label: 'tab 2 label',
+      component: <p>tab body 2</p>,
+    },
+  ],
+};
+
+export const StartingTab = Template.bind({});
+StartingTab.args = {
+  defaultIndex: 1,
+  tabs: [
+    {
+      label: 'tab 1 label',
+      component: <p>tab body 1</p>,
+    },
+    {
+      label: 'tab 2 label',
+      component: <p>We start on the second tab using defaultIndex=1</p>,
+    },
+  ],
+};
