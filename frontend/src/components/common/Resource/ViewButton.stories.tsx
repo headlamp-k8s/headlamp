b3e50cb5a5224586d0e736e5f8a2b79a580415dd
René Dudfield <rene@kinvolk.io>
2021-02-19 17:01:25 +0100

    frontend: Add Resource/ViewButton.stories

---
+++ frontend/src/components/common/Resource/ViewButton.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+import ViewButton from './ViewButton';
+import { ViewButtonProps } from './ViewButton';
+
+export default {
+  title: 'Resource/ViewButton',
+  component: ViewButton,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<ViewButtonProps> = args => <ViewButton {...args} />;
+
+export const View = Template.bind({});
+View.args = {
+  item: {
+    jsonData: {},
+  },
+};
+
+export const ViewOpen = Template.bind({});
+ViewOpen.args = {
+  item: {
+    jsonData: {},
+  },
+  initialToggle: true,
+};
--- frontend/src/components/common/Resource/ViewButton.tsx
+++ frontend/src/components/common/Resource/ViewButton.tsx
@@ -6,12 +6,14 @@ import { KubeObject } from '../../../lib/k8s/cluster';
 import EditorDialog from './EditorDialog';
 
 export interface ViewButtonProps {
+  /** The item we want to view */
   item: KubeObject;
+  /** If we want to have the view open by default */
+  initialToggle?: boolean;
 }
 
-function ViewButton(props: ViewButtonProps) {
-  const { item } = props;
-  const [toggle, setToggle] = React.useState(false);
+function ViewButton({ item, initialToggle = false }: ViewButtonProps) {
+  const [toggle, setToggle] = React.useState(initialToggle);
   function handleButtonClick() {
     setToggle(toggle => !toggle);
   }
