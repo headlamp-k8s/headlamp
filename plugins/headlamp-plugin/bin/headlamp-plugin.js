c30162d5eaefed9a72497f0d8efc786365d6a638
René Dudfield <rene@kinvolk.io>
2021-03-17 22:10:49 +0100

    headlamp-plugin: Add a --watch for development

--- plugins/headlamp-plugin/bin/headlamp-plugin.js
+++ plugins/headlamp-plugin/bin/headlamp-plugin.js
@@ -4,6 +4,15 @@
 
 const webpack = require('webpack');
 const config = require('../config/webpack.config');
+const args = require('minimist')(process.argv.slice(2))
+
+if (args['watch']) {
+  config.watch = true;
+  config.mode = 'development';
+  process.env['BABEL_ENV'] = 'development';
+} else {
+  process.env['BABEL_ENV'] = 'production';
+}
 
 // Compile src and show any warnings + errors
 webpack(config, (err, stats) => {
@@ -11,7 +20,6 @@ webpack(config, (err, stats) => {
     console.log(err);
     return;
   }
-
   const printList = {
     'Warnings': stats.compilation.warnings,
     'Errors': stats.compilation.errors,
