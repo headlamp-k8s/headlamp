d5bf428b6a39d1d48f0e4f94609c082951b02977
René Dudfield <rene@kinvolk.io>
2021-05-06 23:50:19 +0200

    frontend, a11y: Add @axe-core/react to console.log a11y issues

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
