0fa81ac87e6934d4aa80df29dee23c683afc991b
Rene Dudfield <rene@kinvolk.io>
2021-06-09 10:51:03 +0200

    frontend: Only allow 'en' locale translation
    Until there are other ones ready.

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -31,6 +31,8 @@ i18next
   .init({
     debug: process.env.NODE_ENV === 'development',
     fallbackLng: 'en',
+    supportedLngs: ['en'],
+    // nonExplicitSupportedLngs: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
       format: function (value, format, lng) {
