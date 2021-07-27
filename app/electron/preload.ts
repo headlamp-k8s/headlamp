e3a66c393c4ed5e7c96f183d4ce9a5a9c69aba0f
Ashu Ghildiyal <ashu@kinvolk.io>
2021-07-27 10:29:47 +0530

    frontend: Use store to save the app version, send release info to DOM
    This patch also adds a new channel show_release_notes through which
    we will be sending release notes whenenever the app is started for the
    first time

--- app/electron/preload.ts
+++ app/electron/preload.ts
@@ -4,13 +4,13 @@ import { contextBridge, ipcRenderer } from 'electron';
 contextBridge.exposeInMainWorld('desktopApi', {
   send: (channel, data) => {
     // whitelist channels
-    const validChannels = ['update_available', 'locale'];
+    const validChannels = ['update_available', 'locale', 'show_release_notes'];
     if (validChannels.includes(channel)) {
       ipcRenderer.send(channel, data);
     }
   },
   receive: (channel, func) => {
-    const validChannels = ['update_available', 'locale'];
+    const validChannels = ['update_available', 'locale', 'show_release_notes'];
     if (validChannels.includes(channel)) {
       // Deliberately strip event as it includes `sender`
       ipcRenderer.on(channel, ({}, ...args) => func(...args));
