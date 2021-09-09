f41ef5f4a18f06ee693edcfe2f70a59f5b8c856a
Ashu Ghildiyal <ashu@kinvolk.io>
2021-09-09 16:47:00 +0530

    plugin: Add registerDetailsViewSection to Registery type class.

--- plugins/headlamp-plugin/types/plugin/registry.d.ts
+++ plugins/headlamp-plugin/types/plugin/registry.d.ts
@@ -1,5 +1,12 @@
 /// <reference types="react" />
 import { Route } from '../lib/router';
+
+interface SectionFuncProps {
+    title: string;
+    component: (props: { resource: any }) => JSX.Element | null;
+}
+export type sectionFunc = (resource: any) => SectionFuncProps | null | undefined;
+
 export default class Registry {
     registerSidebarItem(parentName: string, itemName: string, itemLabel: string, url: string, opts?: {
         useClusterURL: boolean;
@@ -7,4 +14,5 @@ export default class Registry {
     registerRoute(routeSpec: Route): void;
     registerDetailsViewHeaderAction(actionName: string, actionFunc: (...args: any[]) => JSX.Element | null): void;
     registerAppBarAction(actionName: string, actionFunc: (...args: any[]) => JSX.Element | null): void;
+    registerDetailsViewSection(sectionName: string, sectionFunc: sectionFunc): void;
 }
