0ba76f1c384e9692c91e417455c60173af46c684
René Dudfield <rene@kinvolk.io>
2021-09-23 13:25:45 +0200

    plugins: Add test for headlamp-plugin extract
    - print bash commands for easier debugging
    - improve build test, so it runs headlamp-plugin build

--- plugins/headlamp-plugin/test-headlamp-plugin.sh
+++ plugins/headlamp-plugin/test-headlamp-plugin.sh
@@ -3,7 +3,11 @@
 # Assumes being run within the plugins/headlamp-plugin folder
 
 set -e
+set -o xtrace
+
 rm -rf headlamp-myfancy
+rm -rf .plugins/headlamp-myfancy
+
 
 npm install
 # Use "link" to test the repo version of the headlamp-plugin tool.
@@ -11,8 +15,12 @@ npm link
 node bin/headlamp-plugin.js create headlamp-myfancy
 cd headlamp-myfancy
 npm link @kinvolk/headlamp-plugin
-npm run build
+
+# test headlamp-plugin build
+node ../bin/headlamp-plugin.js build
 stat dist/main.js
+
+# test headlamp-plugin build folder
 cd ..
 rm -rf headlamp-myfancy
 node bin/headlamp-plugin.js create headlamp-myfancy
@@ -21,3 +29,7 @@ npm link @kinvolk/headlamp-plugin
 cd ..
 node bin/headlamp-plugin.js build headlamp-myfancy
 stat headlamp-myfancy/dist/main.js
+
+# test extraction works
+node bin/headlamp-plugin.js extract ./ .plugins
+stat .plugins/headlamp-myfancy/main.js
