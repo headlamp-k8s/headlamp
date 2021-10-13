af2da41847e62e251e3f8fc9a7d6d40c07c12cd0
Ashu Ghildiyal <ashu@kinvolk.io>
2021-10-13 16:55:28 +0530

    frontend: Consider routes from redux store while creating route URL

--- frontend/src/lib/router.tsx
+++ frontend/src/lib/router.tsx
@@ -45,6 +45,7 @@ import PersistentVolumeList from '../components/storage/VolumeList';
 import WorkloadDetails from '../components/workload/Details';
 import WorkloadOverview from '../components/workload/Overview';
 import LocaleSelect from '../i18n/LocaleSelect/LocaleSelect';
+import store from '../redux/stores/store';
 import CronJob from './k8s/cronJob';
 import Deployment from './k8s/deployment';
 import Job from './k8s/job';
@@ -432,7 +433,8 @@ export interface RouteURLProps {
 }
 
 export function createRouteURL(routeName: string, params: RouteURLProps = {}) {
-  const route = getRoute(routeName);
+  const storeRoutes = store.getState().ui.routes;
+  const route = getRoute(routeName) || (storeRoutes && storeRoutes[routeName]);
 
   if (!route) {
     return '';
