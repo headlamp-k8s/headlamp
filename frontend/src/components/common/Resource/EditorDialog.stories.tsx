3b687040ee4f634a4ea71f83ed919994a9f0cf3a
Ashu Ghildiyal <ashu@kinvolk.io>
2021-11-09 23:42:23 +0530

    frontend: Add story for EditorDialog

---
+++ frontend/src/components/common/Resource/EditorDialog.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { EditorDialog, EditorDialogProps } from '..';
+
+export default {
+  title: 'Resource/EditorDialog',
+  component: EditorDialog,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<EditorDialogProps> = args => {
+  return (
+    <EditorDialog
+      {...args}
+      item={{
+        metadata: {
+          name: 'Dummy_Name',
+          uid: 'UNIQUE_ID',
+          resourceVersion: '0.0.0',
+          selfLink: 'https://',
+          creationTimestamp: '10/12/2021',
+        },
+        kind: 'DUMMY_KUBE_KIND',
+      }}
+    />
+  );
+};
+
+export const EditorDialogWithResource = Template.bind({});
