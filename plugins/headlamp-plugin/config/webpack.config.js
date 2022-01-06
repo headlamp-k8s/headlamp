71ed77d28a336bec3de276aed2724d4eed6adbe9
Joaquim Rocha <joaquim@kinvolk.io>
2022-01-06 18:23:19 +0000

    headlamp-plugin: Add shared npm modules as external ones in webpack
    This will allow plugin writers to import React and other modules
    without having to install them and without having webpack bundle them
    as they will be available when importing the plugin's code in Headlamp.

--- plugins/headlamp-plugin/config/webpack.config.js
+++ plugins/headlamp-plugin/config/webpack.config.js
@@ -1,3 +1,17 @@
+const externalModules = {
+  '@material-ui/core': 'pluginLib.MuiCore',
+  '@material-ui/styles': 'pluginLib.MuiStyles',
+  'react': 'pluginLib.React',
+  'recharts': 'pluginLib.Recharts',
+  'react-router': 'pluginLib.ReactRouter',
+  'react-redux': 'pluginLib.ReactRedux',
+  '@iconify/react': 'pluginLib.Iconify',
+  'lodash': 'pluginLib.Lodash',
+  'notistack': 'pluginLib.Notistack',
+  '@kinvolk/headlamp-plugin/lib/CommonComponents': 'pluginLib.CommonComponents',
+  '@kinvolk/headlamp-plugin/lib': 'pluginLib',
+};
+
 module.exports = {
   mode: 'production',
   resolveLoader: {
@@ -5,6 +19,26 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  externals: function ({ request }, callback) {
+    const pluginLib = externalModules[request];
+    // For cases like: import { Grid } from '@material-ui/core'
+    if (!!pluginLib) {
+      return callback(null, pluginLib);
+    }
+
+    // For cases like:
+    // import Grid from '@material-ui/core/Grid' -> const Grid = pluginLib.MuiCore["Grid"];
+    for (let importModule of Object.keys(externalModules)) {
+      const modulePrefix = importModule + '/';
+      if (request.startsWith(modulePrefix)) {
+        const submodule = request.replace(modulePrefix, '').replace(/\/+/g, '');
+        return callback(null, `${externalModules[importModule]}["${submodule}"]`);
+      }
+    }
+
+    // Default
+    callback();
   },
   module: {
     rules:[{
