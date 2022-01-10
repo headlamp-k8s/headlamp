45c42084b32278ccb48c071aefe291a4e5500637
Joaquim Rocha <joaquim@kinvolk.io>
2022-01-10 18:39:22 +0000

    headlamp-plugin: Add lib folder with files to import by plugins

---
+++ plugins/headlamp-plugin/lib/index.ts
@@ -0,0 +1,11 @@
+import * as K8s from '../types/lib/k8s';
+import * as ApiProxy from '../types/lib/k8s/apiProxy';
+import * as Utils from '../types/lib/util';
+import * as CommonComponents from './CommonComponents';
+
+export {
+  ApiProxy,
+  K8s,
+  CommonComponents,
+  Utils,
+};
