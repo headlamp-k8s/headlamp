03b0b743839ded68addc93db5eb913c4173c44b9
Joaquim Rocha <joaquim@kinvolk.io>
2022-01-11 16:38:36 +0000

    headlamp-plugin: Export the Registry type from the plugin lib

--- plugins/headlamp-plugin/lib/index.ts
+++ plugins/headlamp-plugin/lib/index.ts
@@ -1,6 +1,7 @@
 import * as K8s from '../types/lib/k8s';
 import * as ApiProxy from '../types/lib/k8s/apiProxy';
 import * as Utils from '../types/lib/util';
+import Registry from '../types/plugin/registry';
 import * as CommonComponents from './CommonComponents';
 
 export {
@@ -8,4 +9,5 @@ export {
   K8s,
   CommonComponents,
   Utils,
+  Registry
 };
