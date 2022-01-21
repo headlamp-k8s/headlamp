c688a37a0e2acb8c8b1ba403d3b4e7a80ab527c3
René Dudfield <renedudfield@microsoft.com>
2022-01-21 12:22:48 +0100

    frontend: Upgrade CRA, webpack5, storybook

--- frontend/src/storybook.test.tsx
+++ frontend/src/storybook.test.tsx
@@ -1,10 +1,28 @@
 import initStoryshots, { Stories2SnapsConverter } from '@storybook/addon-storyshots';
 import * as rtl from '@testing-library/react';
+import path from 'path';
 import React from 'react';
 
+/**
+ * The storyshot addon has some bug where the path is src/
+ *  and the cwd is src, so it makes src/src folders.
+ */
+class OurConverter extends Stories2SnapsConverter {
+  getStoryshotFile(fileName: string) {
+    const name = super.getStoryshotFile(fileName);
+    if (name.startsWith('src')) {
+      return path.join('..', name);
+    }
+    return name;
+  }
+}
+
 initStoryshots({
+  stories2snapsConverter: new OurConverter(),
+
   test: ({ story: { storyFn: Story }, context }) => {
-    const converter = new Stories2SnapsConverter();
+    // We use React Testing library here, and our custom converter.
+    const converter = new OurConverter();
     const snapshotFilename = converter.getSnapshotFileName(context);
     const rendered = rtl.render(
       <div id="root">
@@ -15,7 +33,7 @@ initStoryshots({
   },
   snapshotSerializers: [
     {
-      print: (val, serialize) => {
+      print: (val: any, serialize) => {
         const root = val.container.firstChild;
         return serialize(root);
       },
