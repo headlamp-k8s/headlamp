13f2247aceb68f7acaaf42270bad12061042412d
René Dudfield <renedudfield@microsoft.com>
2022-02-10 16:23:46 +0100

    plugins: Add test for headlamp-plugin format

--- plugins/headlamp-plugin/test-headlamp-plugin.sh
+++ plugins/headlamp-plugin/test-headlamp-plugin.sh
@@ -7,14 +7,20 @@ set -o xtrace
 
 rm -rf headlamp-myfancy
 rm -rf .plugins/headlamp-myfancy
+rm -f kinvolk-headlamp-plugin-*.tgz
 
 
 npm install
+
+# Make a package file of headlamp-plugin we can test
+npm run build
+npm pack
+
 # Use "link" to test the repo version of the headlamp-plugin tool.
 npm link
-node bin/headlamp-plugin.js create headlamp-myfancy
+node bin/headlamp-plugin.js create headlamp-myfancy --link
 cd headlamp-myfancy
-npm link @kinvolk/headlamp-plugin
+npm install ../kinvolk-headlamp-plugin-*.tgz
 
 # test headlamp-plugin build
 node ../bin/headlamp-plugin.js build
@@ -23,9 +29,9 @@ stat dist/main.js
 # test headlamp-plugin build folder
 cd ..
 rm -rf headlamp-myfancy
-node bin/headlamp-plugin.js create headlamp-myfancy
+node bin/headlamp-plugin.js create headlamp-myfancy --link
 cd headlamp-myfancy
-npm link @kinvolk/headlamp-plugin
+npm install ../kinvolk-headlamp-plugin-*.tgz
 cd ..
 node bin/headlamp-plugin.js build headlamp-myfancy
 stat headlamp-myfancy/dist/main.js
@@ -33,3 +39,10 @@ stat headlamp-myfancy/dist/main.js
 # test extraction works
 node bin/headlamp-plugin.js extract ./ .plugins
 stat .plugins/headlamp-myfancy/main.js
+
+# test format command and that default code is formatted correctly
+rm -rf headlamp-myfancy
+node bin/headlamp-plugin.js create headlamp-myfancy --link
+cd headlamp-myfancy
+npm install ../kinvolk-headlamp-plugin-*.tgz
+npm run format
