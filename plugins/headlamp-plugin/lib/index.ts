50e8bd9108de51be5cf46e5bccf5657c87d104dd
Joaquim Rocha <joaquim@kinvolk.io>
2022-03-29 17:31:58 +0100

    headlamp-plugin: Allow to use lowercase k8s in imports
    The reason for this is that we use the module in lowercase inside
    Headlamp's source code, so plugin developers may also do that by
    copying. This way we may avoid misunderstandings.

--- plugins/headlamp-plugin/lib/index.ts
+++ plugins/headlamp-plugin/lib/index.ts
@@ -5,4 +5,5 @@ import { Headlamp, Plugin } from '../types/plugin/lib';
 import Registry from '../types/plugin/registry';
 import * as CommonComponents from './CommonComponents';
 
-export { ApiProxy, K8s, CommonComponents, Utils, Plugin, Registry, Headlamp };
+// We export k8s (lowercase) since someone may use it as we do in the Headlamp source code.
+export { ApiProxy, K8s, K8s as k8s, CommonComponents, Utils, Plugin, Registry, Headlamp };
