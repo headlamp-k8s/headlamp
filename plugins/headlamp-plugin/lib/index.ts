0f5eff3561fea3ebbac729ab9157e8452bc0ad96
ashu8912 <aghildiyal@microsoft.com>
2022-04-04 12:07:38 +0530

    frontend: Add router to pluginLib
    We want all the route related methods to be used by plugin developers
    thus exporting this in the pluginLib makes sense.

--- plugins/headlamp-plugin/lib/index.ts
+++ plugins/headlamp-plugin/lib/index.ts
@@ -1,6 +1,7 @@
 import * as K8s from '../types/lib/k8s';
 import * as ApiProxy from '../types/lib/k8s/apiProxy';
 import * as Notification from '../types/lib/Notification';
+import * as Router from '../types/lib/router';
 import * as Utils from '../types/lib/util';
 import { Headlamp, Plugin } from '../types/plugin/lib';
 import Registry from '../types/plugin/registry';
@@ -13,6 +14,7 @@ export {
   K8s as k8s,
   CommonComponents,
   Utils,
+  Router,
   Plugin,
   Registry,
   Headlamp,
