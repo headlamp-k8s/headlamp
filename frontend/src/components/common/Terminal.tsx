e292d792d4192abfb3e4275e04e812788ad3aeff
Joaquim Rocha <joaquim.rocha@microsoft.com>
2022-05-26 20:45:14 +0100

    frontend: Resize Terminal when the window is resized
    This patch improves the user experience when using the terminal by
    making sure that it is resized when its container's size is also
    changed. This reduces the use of scrollbars to only the actual
    terminal ones.
    
    Co-Authored-By: fondoger <fondoger@outlook.com>.

--- frontend/src/components/common/Terminal.tsx
+++ frontend/src/components/common/Terminal.tsx
@@ -1,11 +1,11 @@
 import 'xterm/css/xterm.css';
+import { Box } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog, { DialogProps } from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import FormControl from '@material-ui/core/FormControl';
-import Grid from '@material-ui/core/Grid';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -19,13 +19,35 @@ import Pod from '../../lib/k8s/pod';
 const decoder = new TextDecoder('utf-8');
 const encoder = new TextEncoder();
 
-const useStyle = makeStyles(() => ({
+const useStyle = makeStyles(theme => ({
   dialogContent: {
-    height: '80%',
-    minHeight: '80%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    '& .xterm ': {
+      height: '500px', // So the terminal doesn't stay shrunk when shrinking vertically and maximizing again.
+      '& .xterm-viewport': {
+        width: 'initial !important', // BugFix: https://github.com/xtermjs/xterm.js/issues/3564#issuecomment-1004417440
+      },
+    },
+    '& #xterm-container': {
+      overflow: 'hidden',
+      width: '100%',
+      '& .terminal.xterm': {
+        padding: 10,
+      },
+    },
   },
   containerFormControl: {
     minWidth: '11rem',
+  },
+  terminalBox: {
+    paddingTop: theme.spacing(1),
+    flex: 1,
+    width: '100%',
+    overflow: 'hidden',
+    display: 'flex',
+    flexDirection: 'column-reverse',
   },
 }));
 
@@ -57,7 +79,11 @@ export default function Terminal(props: TerminalProps) {
     xterm.open(containerRef);
 
     xterm.onData(data => {
-      send(data);
+      send(0, data);
+    });
+
+    xterm.onResize(size => {
+      send(4, `{"Width":${size.cols},"Height":${size.rows}}`);
     });
 
     // Allow copy/paste in terminal
@@ -79,22 +105,34 @@ export default function Terminal(props: TerminalProps) {
     fitAddon.fit();
   }
 
-  function send(data: string) {
+  function send(channel: number, data: string) {
     const socket = execRef.current.getSocket();
 
     // We should only send data if the socket is ready.
     if (!socket || socket.readyState !== 1) {
-      console.debug('Socket not ready...', socket);
+      console.debug('Could not send data to exec: Socket not ready...', socket);
       return;
     }
 
     const encoded = encoder.encode(data);
-    const buffer = new Uint8Array([0, ...encoded]);
+    const buffer = new Uint8Array([channel, ...encoded]);
 
     socket.send(buffer);
   }
 
+  // Channels:
+  // 0: stdin
+  // 1: stdout
+  // 2: stderr
+  // 3: server error
+  // 4: resize channel
   function onData(xterm: XTerminal, bytes: ArrayBuffer) {
+    // Only show data from stdout, stderr and server error channel.
+    const channel = new Int8Array(bytes.slice(0, 1))[0];
+    if (channel < 1 || channel > 3) {
+      return;
+    }
+
     // The first byte is discarded because it just identifies whether
     // this data is from stderr, stdout, or stdin.
     const data = bytes.slice(1);
@@ -102,6 +140,16 @@ export default function Terminal(props: TerminalProps) {
 
     if (bytes.byteLength < 2) {
       return;
+    }
+
+    // Send resize command to server once connection is establised.
+    if (!(xterm as any).connected && !!text) {
+      xterm.clear();
+      (async function () {
+        send(4, `{"Width":${xterm.cols},"Height":${xterm.rows}}`);
+      })();
+      (xterm as any).connected = true;
+      console.debug('Terminal is now connected');
     }
 
     xterm.write(text);
@@ -145,11 +193,18 @@ export default function Terminal(props: TerminalProps) {
         setupTerminal(terminalContainerRef, xtermRef.current, fitAddonRef.current);
       })();
 
+      const handler = () => {
+        fitAddonRef.current.fit();
+      };
+
+      window.addEventListener('resize', handler);
+
       return function cleanup() {
         xtermRef.current.dispose();
         if (execRef.current) {
           execRef.current.cancel();
         }
+        window.removeEventListener('resize', handler);
       };
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -174,31 +229,33 @@ export default function Terminal(props: TerminalProps) {
     <Dialog maxWidth="lg" scroll="paper" fullWidth onClose={onClose} keepMounted {...other}>
       <DialogTitle>{t('Terminal: {{ itemName }}', { itemName: item.metadata.name })}</DialogTitle>
       <DialogContent className={classes.dialogContent}>
-        <Grid container direction="column" spacing={1}>
-          <Grid item>
-            <FormControl className={classes.containerFormControl}>
-              <InputLabel shrink id="container-name-chooser-label">
-                {t('glossary|Container')}
-              </InputLabel>
-              <Select
-                labelId="container-name-chooser-label"
-                id="container-name-chooser"
-                value={container !== null ? container : getDefaultContainer()}
-                onChange={handleContainerChange}
-              >
-                {item &&
-                  item.spec.containers.map(({ name }) => (
-                    <MenuItem value={name} key={name}>
-                      {name}
-                    </MenuItem>
-                  ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item>
-            <div id="xterm" ref={x => setTerminalContainerRef(x)} className="terminal" />
-          </Grid>
-        </Grid>
+        <Box>
+          <FormControl className={classes.containerFormControl}>
+            <InputLabel shrink id="container-name-chooser-label">
+              {t('glossary|Container')}
+            </InputLabel>
+            <Select
+              labelId="container-name-chooser-label"
+              id="container-name-chooser"
+              value={container !== null ? container : getDefaultContainer()}
+              onChange={handleContainerChange}
+            >
+              {item &&
+                item.spec.containers.map(({ name }) => (
+                  <MenuItem value={name} key={name}>
+                    {name}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box className={classes.terminalBox}>
+          <div
+            id="xterm-container"
+            ref={x => setTerminalContainerRef(x)}
+            style={{ flex: 1, display: 'flex', flexDirection: 'column-reverse' }}
+          />
+        </Box>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">
