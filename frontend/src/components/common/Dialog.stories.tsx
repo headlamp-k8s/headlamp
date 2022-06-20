8d6ed176d37457e4d87a89fde77d0ad35cea09e6
Joaquim Rocha <joaquim.rocha@microsoft.com>
2022-06-20 10:51:53 +0100

    frontend: Add stories for the new Dialog

--- frontend/src/components/common/Dialog.stories.tsx
+++ frontend/src/components/common/Dialog.stories.tsx
@@ -1,6 +1,11 @@
+import { DialogContent, Typography } from '@material-ui/core';
 import { Meta, Story } from '@storybook/react/types-6-0';
-import React from 'react';
-import { ConfirmDialog as ConfirmDialogComponent, ConfirmDialogProps } from './Dialog';
+import {
+  ConfirmDialog as ConfirmDialogComponent,
+  ConfirmDialogProps,
+  Dialog as DialogComponent,
+  DialogProps,
+} from './Dialog';
 
 // NOTE: Dialog.tsx should maybe be renamed ConfirmDialog.tsx
 
@@ -13,18 +18,53 @@ export default {
   },
 } as Meta;
 
-const Template: Story<ConfirmDialogProps> = args => <ConfirmDialogComponent {...args} />;
+const ConfirmTemplate: Story<ConfirmDialogProps> = args => <ConfirmDialogComponent {...args} />;
 
-export const ConfirmDialog = Template.bind({});
+export const ConfirmDialog = ConfirmTemplate.bind({});
 ConfirmDialog.args = {
   open: true,
   title: 'A fine title',
   description: 'A really good description.',
 };
 
-export const ConfirmDialogClosed = Template.bind({});
+export const ConfirmDialogClosed = ConfirmTemplate.bind({});
 ConfirmDialogClosed.args = {
   open: false,
   title: 'A fine title',
   description: 'A really good description.',
+};
+
+const Template: Story<DialogProps> = args => (
+  <DialogComponent {...args}>
+    <DialogContent>
+      <Typography>Some content here</Typography>
+    </DialogContent>
+  </DialogComponent>
+);
+
+export const Dialog = Template.bind({});
+Dialog.args = {
+  open: true,
+  title: 'A fine title',
+};
+
+export const DialogWithCloseButton = Template.bind({});
+DialogWithCloseButton.args = {
+  open: true,
+  title: 'A fine title',
+};
+
+export const DialogWithFullScreenButton = Template.bind({});
+DialogWithFullScreenButton.args = {
+  open: true,
+  title: 'A fine title',
+  withFullScreen: true,
+};
+
+export const DialogAlreadyInFullScreen = Template.bind({});
+DialogAlreadyInFullScreen.args = {
+  open: true,
+  title: 'A fine title',
+  withFullScreen: true,
+  fullScreen: true,
 };
