a76cdf5def402c9c95d3a5175b8609dfde484665
Joaquim Rocha <joaquim.rocha@microsoft.com>
2022-08-01 17:37:43 +0100

    app: Use package.json info to build Windows MSI
    This patch uses the info we have already on package.json, official one
    as well as the electrno-builder's one, to build the MSI installer.
    
    This means we get an MSI file whose name is consistent with the ones
    we generate from electron-builder.

--- app/scripts/build-msi.js
+++ app/scripts/build-msi.js
@@ -1,25 +1,53 @@
 // You need to install wix tool set first
 // Reference https://ourcodeworld.com/articles/read/927/how-to-create-a-msi-installer-in-windows-for-an-electron-framework-application
 const { MSICreator } = require('electron-wix-msi');
+const fs = require('fs');
 const path = require('path');
+const info = require('../package.json');
 
 const APP_DIR = path.resolve(__dirname, '../dist/win-unpacked');
 const OUT_DIR = path.resolve(__dirname, '../dist');
+const ARCH = 'x64';
 
-const msiCreator = new MSICreator({
+const nameOptions = {
+  productName: info.productName,
+  version: info.version,
+  os: 'win',
+  arch: ARCH,
+};
+
+// Generate the exe name from electron-builder's artifactName
+let installerName = info.build.artifactName.split('.')[0];
+Object.entries(nameOptions).forEach(([key, value]) => {
+  installerName = installerName.replace(`\${${key}}`, value);
+});
+installerName += '.msi';
+
+// For reference: https://github.com/felixrieseberg/electron-wix-msi#configuration
+const msiOptions = {
   appDirectory: APP_DIR,
   outputDirectory: OUT_DIR,
-  description: 'Easy-to-use and extensible Kubernetes web UI',
-  exe: process.env.npm_package_name,
-  name: process.env.npm_package_name,
-  manufacturer: 'Kinvolk',
-  version: process.env.npm_package_version,
+  description: info.description,
+  exe: info.name, // Name of the executable to launch the app, not the final installer.
+  name: info.productName,
+  manufacturer: info.author.name,
+  version: info.version,
   appIconPath: path.resolve(__dirname, '../build/icons/icon.ico'),
   ui: {
     chooseDirectory: true,
   },
-});
+};
 
-msiCreator.create().then(function () {
-  msiCreator.compile();
+console.info('Generating MSI with the following options:', msiOptions);
+
+const msiCreator = new MSICreator(msiOptions);
+
+msiCreator.create().then(async () => {
+  await msiCreator.compile();
+
+  // Rename the executable to the full name we want.
+  const installerPath = path.join(OUT_DIR, installerName);
+  fs.renameSync(path.join(OUT_DIR, msiOptions.exe + '.msi'), installerPath);
+
+  console.info('Created .msi installer at: ', installerPath);
 });
