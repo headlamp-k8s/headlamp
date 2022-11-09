698cc8dbb861b4a63977f7fb2f70184491eb81b6
René Dudfield <renedudfield@microsoft.com>
2022-11-09 15:27:50 +0100

    headlamp-plugin pod-counter: Add Message.stories.tsx example story

---
+++ plugins/examples/pod-counter/src/Message.tsx
@@ -0,0 +1,33 @@
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyle = makeStyles(() => ({
+  pods: {
+    fontStyle: 'italic',
+  },
+}));
+
+export interface MessageProps {
+  /** String to display. */
+  msg: string;
+  /** True if there's an error. */
+  error: boolean;
+}
+
+/**
+ * For showing our action item message.
+ * 
+ * This is a pure presentational component to make it 
+ * easier to use in storybook.
+ * 
+ * See https://storybook.js.org/docs/web-components/writing-stories/build-pages-with-storybook
+ *
+ */
+export default function Message({ msg, error }: MessageProps) {
+  const classes = useStyle();
+  return (
+    <Typography color="textPrimary" className={classes.pods}>
+      {!error ? `# Pods: ${msg}` : 'Uh, pods!?'}
+    </Typography>
+  );
+}
--- plugins/examples/pod-counter/src/index.tsx
+++ plugins/examples/pod-counter/src/index.tsx
@@ -1,23 +1,10 @@
 import { K8s, registerAppBarAction } from '@kinvolk/headlamp-plugin/lib';
-import { Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
-
-const useStyle = makeStyles(() => ({
-  pods: {
-    fontStyle: 'italic',
-  },
-}));
+import Message from './Message';
 
 function PodCounter() {
-  const classes = useStyle();
   const [pods, error] = K8s.ResourceClasses.Pod.useList();
   const msg = pods === null ? 'Loading…' : pods.length.toString();
-
-  return (
-    <Typography color="textPrimary" className={classes.pods}>
-      {!error ? `# Pods: ${msg}` : 'Uh, pods!?'}
-    </Typography>
-  );
+  return <Message msg={msg} error={error} />
 }
 
 registerAppBarAction(PodCounter);
