f9fbcab86eb227f6a921b210cc1f43e4178cade9
yolossn <sannagaraj@microsoft.com>
2023-01-07 12:29:40 +0530

    backend: Add ability to setup cluster from kubeconfig
    This patch adds the ability to setup a dynamic cluster
    by providing base64 encoded kubeconfig
    
    Signed-off-by: yolossn <sannagaraj@microsoft.com>

--- backend/cmd/cluster.go
+++ backend/cmd/cluster.go
@@ -24,16 +24,16 @@ type Cluster struct {
 }
 
 type ClusterReq struct {
-	Name   string `json:"name"`
-	Server string `json:"server"`
+	Name   *string `json:"name"`
+	Server *string `json:"server"`
 	// InsecureSkipTLSVerify skips the validity check for the server's certificate.
 	// This will make your HTTPS connections insecure.
 	// +optional
 	InsecureSkipTLSVerify bool `json:"insecure-skip-tls-verify,omitempty"`
 	// CertificateAuthorityData contains PEM-encoded certificate authority certificates. Overrides CertificateAuthority
 	// +optional
-	CertificateAuthorityData []byte                 `json:"certificate-authority-data,omitempty"`
-	Metadata                 map[string]interface{} `json:"meta_data"`
+	CertificateAuthorityData []byte  `json:"certificate-authority-data,omitempty"`
+	KubeConfig               *string `json:"kubeconfig,omitempty"`
 }
 
 func GetClusterOidcConfig(clusterName string) (*OidcConfig, error) {
