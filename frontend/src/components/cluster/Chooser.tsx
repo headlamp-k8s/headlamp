694f80a1a5050b55b6655b1b57d9722976985fb3
yolossn <sannagaraj@microsoft.com>
2023-01-09 15:07:33 +0530

    frontend: Add Load from file button to ClusterChooser
    This patch adds load from file butotn to cluster chooser
    the user can upload a file and select the clusters to
    be loaded.
    
    Signed-off-by: yolossn <sannagaraj@microsoft.com>

--- frontend/src/components/cluster/Chooser.tsx
+++ frontend/src/components/cluster/Chooser.tsx
@@ -1,5 +1,5 @@
 import { Icon, InlineIcon } from '@iconify/react';
-import { IconButton } from '@material-ui/core';
+import { Button, Checkbox, FormControl, IconButton } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Card from '@material-ui/core/Card';
@@ -8,14 +8,17 @@ import Container from '@material-ui/core/Container';
 import Dialog, { DialogProps } from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import * as yaml from 'js-yaml';
 import _ from 'lodash';
-import React, { isValidElement, PropsWithChildren } from 'react';
+import React, { isValidElement, PropsWithChildren, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
@@ -23,10 +26,12 @@ import { generatePath } from 'react-router';
 import { useHistory } from 'react-router-dom';
 import helpers from '../../helpers';
 import { useClustersConf } from '../../lib/k8s';
+import { setCluster } from '../../lib/k8s/apiProxy';
 import { Cluster } from '../../lib/k8s/cluster';
 import { getThemeName } from '../../lib/themes';
 import { getCluster, getClusterPrefixedPath } from '../../lib/util';
 import { setVersionDialogOpen } from '../../redux/actions/actions';
+import { setConfig } from '../../redux/actions/actions';
 import { useTypedSelector } from '../../redux/reducers/reducers';
 import { EmptyContent } from '../common';
 import { DialogTitle } from '../common/Dialog';
@@ -257,6 +262,7 @@ interface ClusterDialogProps extends PropsWithChildren<Omit<DialogProps, 'open' 
   open?: boolean;
   onClose?: (() => void) | null;
   useCover?: boolean;
+  error?: string;
 }
 
 export function ClusterDialog(props: ClusterDialogProps) {
@@ -264,7 +270,7 @@ export function ClusterDialog(props: ClusterDialogProps) {
   const theme = useTheme();
   const { t } = useTranslation('cluster');
   const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-  const { open, onClose = null, useCover = false, children = [], ...otherProps } = props;
+  const { open, onClose = null, useCover = false, children = [], error, ...otherProps } = props;
   // Only used if open is not provided
   const [show, setShow] = React.useState(true);
   const dispatch = useDispatch();
@@ -333,6 +339,9 @@ export function ClusterDialog(props: ClusterDialogProps) {
           }
         </ErrorBoundary>
       </DialogTitle>
+      {error && error !== '' ? (
+        <Box style={{ backgroundColor: 'red', textAlign: 'center', padding: '4px' }}>{error}</Box>
+      ) : null}
       <DialogContent className={classes.chooserDialog}>{children}</DialogContent>
     </Dialog>
   );
@@ -344,8 +353,9 @@ function Chooser(props: ClusterDialogProps) {
   const { open = null, onClose, children = [], ...otherProps } = props;
   // Only used if open is not provided
   const [show, setShow] = React.useState(props.open);
+  const [loadFromKubeConfig, setLoadFromKubeConfig] = useState(false);
+  const [error, setError] = React.useState('');
   const { t } = useTranslation('cluster');
-
   React.useEffect(
     () => {
       if (open !== null && open !== show) {
@@ -402,40 +412,326 @@ function Chooser(props: ClusterDialogProps) {
         onClose={onClose || handleClose}
         aria-labelledby="chooser-dialog-title"
         aria-busy={clusterList.length === 0 && clusters === null}
+        error={error}
         {...otherProps}
       >
-        <DialogTitle id="chooser-dialog-title" focusTitle>
-          {t('Choose a cluster')}
-        </DialogTitle>
-
-        {clusterList.length === 0 ? (
-          <React.Fragment>
-            {clusters === null ? (
-              <>
-                <DialogContentText>{t('Wait while fetching clusters…')}</DialogContentText>
-                <Loader title={t('Loading cluster information')} />
-              </>
-            ) : (
-              <>
-                <DialogContentText>
-                  {t('There seems to be no clusters configured…')}
-                </DialogContentText>
-                <DialogContentText>
-                  {t('Please make sure you have at least one cluster configured.')}
-                </DialogContentText>
-                {helpers.isElectron() && (
-                  <DialogContentText>
-                    {t('Or try running Headlamp with a different kube config.')}
-                  </DialogContentText>
-                )}
-              </>
-            )}
-          </React.Fragment>
+        {!loadFromKubeConfig ? (
+          <>
+            <DialogTitle id="chooser-dialog-title" focusTitle>
+              {t('Choose a cluster')}
+            </DialogTitle>
+            <DialogContent>
+              {clusterList.length === 0 ? (
+                <React.Fragment>
+                  {clusters === null ? (
+                    <>
+                      <DialogContentText>{t('Wait while fetching clusters…')}</DialogContentText>
+                      <Loader title={t('Loading cluster information')} />
+                    </>
+                  ) : (
+                    <>
+                      <DialogContentText>
+                        {t('There seems to be no clusters configured…')}
+                      </DialogContentText>
+                      <DialogContentText>
+                        {t('Please make sure you have at least one cluster configured.')}
+                      </DialogContentText>
+                      {helpers.isElectron() && (
+                        <DialogContentText>
+                          {t('Or try running Headlamp with a different kube config.')}
+                        </DialogContentText>
+                      )}
+                    </>
+                  )}
+                </React.Fragment>
+              ) : (
+                <ClusterList clusters={clusterList} onButtonClick={handleButtonClick} />
+              )}
+              <Box style={{ justifyContent: 'center', display: 'flex' }}>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={() => setLoadFromKubeConfig(true)}
+                >
+                  {t('Load from a file')}
+                </Button>
+              </Box>
+            </DialogContent>
+          </>
         ) : (
-          <ClusterList clusters={clusterList} onButtonClick={handleButtonClick} />
+          <KubeConfigUpload back={() => setLoadFromKubeConfig(false)} setError={setError} />
         )}
+
         {children}
       </ClusterDialog>
+    </Box>
+  );
+}
+
+interface kubeconfig {
+  clusters: Array<{ name: string; cluster: { server: string } }>;
+  users: Array<{ name: string; user: { token: string } }>;
+  contexts: Array<{ name: string; context: { cluster: string; user: string } }>;
+  currentContext: string;
+}
+
+function configWithSelectedClusters(config: kubeconfig, selectedClusters: string[]): kubeconfig {
+  const newConfig = _.cloneDeep(config);
+
+  config.clusters.forEach(cluster => {
+    if (!selectedClusters.includes(cluster.name)) {
+      const indexOfClusterToBeRemoved = newConfig.clusters.findIndex(c => c.name === cluster.name);
+      const clusterToBeRemoved = newConfig.clusters[indexOfClusterToBeRemoved];
+
+      const indexOfContextToBeRemoved = newConfig.contexts.findIndex(
+        c => c.context.cluster === clusterToBeRemoved.name
+      );
+      const contextToBeRemoved = newConfig.contexts[indexOfContextToBeRemoved];
+
+      const indexOfUserToBeRemoved = newConfig.users.findIndex(
+        c => c.name === contextToBeRemoved.context.user
+      );
+
+      // remove user
+      newConfig.clusters.splice(indexOfClusterToBeRemoved, 1);
+      // remove context
+      newConfig.contexts.splice(indexOfContextToBeRemoved, 1);
+      // remove cluster
+      newConfig.users.splice(indexOfUserToBeRemoved, 1);
+    }
+  });
+  newConfig.currentContext = '';
+  return newConfig;
+}
+
+function KubeConfigUpload(props: { back: () => void; setError: (error: string) => void }) {
+  const { back, setError } = props;
+
+  // State can be: load_kubeconfig, selecting_clusters, configuring_clusters, success
+  const [state, setState] = useState('load_kubeconfig');
+  const [fileContent, setFileContent] = useState<kubeconfig>({
+    clusters: [],
+    users: [],
+    contexts: [],
+    currentContext: '',
+  });
+  const [selectedClusters, setSelectedClusters] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (fileContent.clusters.length > 0) {
+      if (fileContent.clusters.length === 1) {
+        setSelectedClusters([fileContent.clusters[0].name]);
+        setState('configuring_clusters');
+        return;
+      }
+      setState('selecting_clusters');
+    }
+    return () => {};
+  }, [fileContent]);
+
+  useEffect(() => {
+    if (state === 'configuring_clusters') {
+      function loadClusters() {
+        const selectedClusterConfig = configWithSelectedClusters(fileContent, selectedClusters);
+        setCluster({ kubeconfig: btoa(yaml.dump(selectedClusterConfig)) })
+          .then(res => {
+            if (res.clusters.length > 0) {
+              dispatch(setConfig(res));
+              setState('success');
+            }
+          })
+          .catch(() => {
+            setError(t('cluster|Error setting up cluster, please upload a valid kubeconfig file'));
+          });
+      }
+      loadClusters();
+    }
+    return () => {};
+  }, [state]);
+
+  const dispatch = useDispatch();
+  const { t } = useTranslation(['cluster', 'frequent']);
+
+  const useStyles = makeStyles({
+    dropzone: {
+      border: 1,
+      borderRadius: 1,
+      borderWidth: 2,
+      borderColor: 'rgba(0, 0, 0)',
+      borderStyle: 'dashed',
+      padding: '20px',
+      margin: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      '&:hover': {
+        borderColor: 'rgba(0, 0, 0, 0.5)',
+      },
+      '&:focus-within': {
+        borderColor: 'rgba(0, 0, 0, 0.5)',
+      },
+    },
+  });
+
+  const classes = useStyles();
+
+  const onDrop = (acceptedFiles: Blob[]) => {
+    setError('');
+    const reader = new FileReader();
+    reader.onerror = () => setError(t("cluster|Couldn't read kubeconfig file"));
+    reader.onload = () => {
+      try {
+        const data = String.fromCharCode.apply(null, [
+          ...new Uint8Array(reader.result as ArrayBuffer),
+        ]);
+        const doc = yaml.load(data) as kubeconfig;
+        setFileContent(doc);
+      } catch (err) {
+        setError(t('cluster|Upload a valid kubeconfig'));
+        return;
+      }
+    };
+    reader.readAsArrayBuffer(acceptedFiles[0]);
+  };
+
+  const { getRootProps, getInputProps, open } = useDropzone({
+    onDrop: onDrop,
+    multiple: false,
+  });
+
+  function handleCheckboxChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.checked) {
+      if (selectedClusters.includes(event.target.name)) {
+        // remove from selected clusters
+        setSelectedClusters(selectedClusters.filter(cluster => cluster !== event.target.name));
+      } else {
+        // add to selected clusters
+        setSelectedClusters([...selectedClusters, event.target.name]);
+      }
+    }
+  }
+
+  function renderSwitch() {
+    switch (state) {
+      case 'load_kubeconfig':
+        return (
+          <Box
+            border={1}
+            borderColor="secondary.main"
+            {...getRootProps()}
+            className={classes.dropzone}
+          >
+            <FormControl>
+              <input {...getInputProps()} />
+              <Icon
+                icon="mdi:upload"
+                style={{ fontSize: '64px', width: 'auto', justifySelf: 'center' }}
+              />
+              <Typography>{t('cluster|Drag&Drop your kubeconfig file here')}</Typography>
+              <Button variant="contained" onClick={() => open}>
+                {t('cluster|Upload File')}
+              </Button>
+            </FormControl>
+          </Box>
+        );
+      case 'selecting_clusters':
+        return (
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <Typography>{t('cluster|Select Clusters')}</Typography>
+            {fileContent.clusters ? (
+              <>
+                <Box
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: '15px',
+                  }}
+                >
+                  <FormControl
+                    style={{
+                      overflowY: 'auto',
+                      height: '100px',
+                      paddingLeft: '10px',
+                      paddingRight: '10px',
+                    }}
+                  >
+                    {fileContent.clusters.map(cluster => {
+                      return (
+                        <FormControlLabel
+                          key={cluster.name}
+                          control={
+                            <Checkbox
+                              value={cluster.name}
+                              name={cluster.name}
+                              onChange={handleCheckboxChange}
+                            />
+                          }
+                          label={cluster.name}
+                        />
+                      );
+                    })}
+                  </FormControl>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => {
+                      setState('configuring_clusters');
+                    }}
+                  >
+                    Next
+                  </Button>
+                </Box>
+              </>
+            ) : null}
+          </Box>
+        );
+      case 'configuring_clusters':
+        return (
+          <Box style={{ textAlign: 'center' }}>
+            <Typography>{t('cluster|Setting up cluster')}</Typography>
+            <Loader title={t('cluster|Setting up cluster')} />
+          </Box>
+        );
+      case 'success':
+        return (
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <Typography>{t('cluster|Cluster setup successfully!')}</Typography>
+            <Box style={{ display: 'flex', justifyContent: 'center' }}>
+              <Button variant="contained" color="primary" onClick={() => back()}>
+                Finish
+              </Button>
+            </Box>
+          </Box>
+        );
+    }
+  }
+
+  return (
+    <Box>
+      {renderSwitch()}
+      {state !== 'success' && (
+        <Box style={{ justifyContent: 'center', display: 'flex' }}>
+          <Button variant="contained" color="primary" onClick={back}>
+            {t('frequent|Cancel')}
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 }
