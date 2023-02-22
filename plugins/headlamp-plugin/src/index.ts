309ccd3dc89c6b0126a4b36c01b691892d69b351
René Dudfield <renedudfield@microsoft.com>
2023-02-22 17:32:48 +0100

    frontend: Move frontend/src/lib/index.ts to plugins/headlamp-plugin/src/index.ts
    Because it's used by plugins only, not Headlamp itself.

--- plugins/headlamp-plugin/src/index.ts
+++ plugins/headlamp-plugin/src/index.ts
@@ -1,1 +1,53 @@
-export * from './lib/index';
+import * as CommonComponents from './components/common';
+import * as K8s from './lib/k8s';
+import * as ApiProxy from './lib/k8s/apiProxy';
+import * as Notification from './lib/notification';
+import * as Router from './lib/router';
+import * as Utils from './lib/util';
+import { Headlamp, Plugin } from './plugin/lib';
+import Registry, {
+  AppLogoProps,
+  ClusterChooserProps,
+  DefaultSidebars,
+  DetailsViewDefaultHeaderActions,
+  DetailsViewSectionProps,
+  registerAppBarAction,
+  registerAppLogo,
+  registerClusterChooser,
+  registerDetailsViewHeaderAction,
+  registerDetailsViewHeaderActionsProcessor,
+  registerDetailsViewSection,
+  registerGetTokenFunction,
+  registerRoute,
+  registerRouteFilter,
+  registerSidebarEntry,
+  registerSidebarEntryFilter,
+} from './plugin/registry';
+
+// We export k8s (lowercase) since someone may use it as we do in the Headlamp source code.
+export {
+  ApiProxy,
+  K8s,
+  K8s as k8s,
+  CommonComponents,
+  Utils,
+  Router,
+  Plugin,
+  Registry,
+  Headlamp,
+  Notification,
+  DetailsViewDefaultHeaderActions,
+  registerAppLogo,
+  registerAppBarAction,
+  registerClusterChooser,
+  registerDetailsViewHeaderAction,
+  registerDetailsViewSection,
+  registerRoute,
+  registerRouteFilter,
+  registerSidebarEntry,
+  registerSidebarEntryFilter,
+  registerDetailsViewHeaderActionsProcessor,
+  registerGetTokenFunction,
+};
+
+export type { AppLogoProps, ClusterChooserProps, DetailsViewSectionProps, DefaultSidebars };
