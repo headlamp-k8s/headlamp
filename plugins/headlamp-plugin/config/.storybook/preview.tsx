59cb45035d554df00e7222f87bbf57cc2a23280a
René Dudfield <renedudfield@microsoft.com>
2023-03-01 13:35:06 +0100

    headlamp-plugin: Remove commented out unused code

--- plugins/headlamp-plugin/config/.storybook/preview.tsx
+++ plugins/headlamp-plugin/config/.storybook/preview.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import themesConf from '@kinvolk/headlamp-plugin/lib/lib/themes';
 import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
-// import { initialize, mswDecorator } from 'msw-storybook-addon';
-// import { rest } from 'msw'
-
-// https://github.com/mswjs/msw-storybook-addon
-// initialize();
 
 const darkTheme = themesConf['dark'];
 const lightTheme = themesConf['light'];
@@ -54,22 +49,4 @@ export const parameters = {
     ],
   },
   actions: { argTypesRegex: '^on[A-Z].*' },
-
-  // // https://github.com/mswjs/msw-storybook-addon#composing-request-handlers
-  // msw: {
-  //   handlers: [
-  //     rest.get('https://api.iconify.design/mdi.json', (_req, res, ctx) => {
-  //       return res(ctx.json({}));
-  //     }),
-  //     rest.get('http://localhost/api/v1/namespaces', (_req, res, ctx) => {
-  //       return res(ctx.json({}));
-  //     }),
-  //     rest.get('http://localhost/api/v1/events', (_req, res, ctx) => {
-  //       return res(ctx.json({}));
-  //     }),
-  //     rest.post('http://localhost/apis/authorization.k8s.io/v1/selfsubjectaccessreviews', (_req, res, ctx) => {
-  //       return res(ctx.json({status: 200}));
-  //     }),
-  //   ]
-  // },
 };
