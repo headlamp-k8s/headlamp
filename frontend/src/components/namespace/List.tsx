2c7902cedd6cbbc2af8660d7607dbfaeef058995
ashu8912 <aghildiyal@microsoft.com>
2023-03-06 17:36:54 +0530

    frontend: Update Namespace list view to consider allowedNamespaces too

--- frontend/src/components/namespace/List.tsx
+++ frontend/src/components/namespace/List.tsx
@@ -1,5 +1,9 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import helpers from '../../helpers';
+import { useCluster } from '../../lib/k8s';
 import Namespace from '../../lib/k8s/namespace';
+import { Link, SimpleTable } from '../common';
 import { StatusLabel } from '../common/Label';
 import ResourceTable from '../common/Resource/ResourceTable';
 import { SectionBox } from '../common/SectionBox';
@@ -7,16 +11,53 @@ import SectionFilterHeader from '../common/SectionFilterHeader';
 
 export default function NamespacesList() {
   const { t } = useTranslation('glossary');
+  const cluster = useCluster();
+  const [allowedNamespaces, setAllowedNamespaces] = React.useState<{ namespace: string }[]>([]);
+  let renderResource = null;
+
+  React.useEffect(() => {
+    if (cluster) {
+      const namespaces = helpers.loadClusterSettings(cluster)?.allowedNamespaces || [];
+      setAllowedNamespaces(namespaces.map(namespace => ({ namespace })));
+    }
+  }, [cluster]);
 
   function makeStatusLabel(namespace: Namespace) {
     const status = namespace.status.phase;
     return <StatusLabel status={status === 'Active' ? 'success' : 'error'}>{status}</StatusLabel>;
   }
 
-  return (
-    <SectionBox
-      title={<SectionFilterHeader title={t('Namespaces')} noNamespaceFilter headerStyle="main" />}
-    >
+  if (allowedNamespaces.length > 0) {
+    renderResource = (
+      <SimpleTable
+        columns={[
+          {
+            label: t('Name'),
+            getter: ({ namespace }) => (
+              <Link
+                routeName={'namespace'}
+                params={{
+                  name: namespace,
+                }}
+              >
+                {namespace}
+              </Link>
+            ),
+          },
+          {
+            label: t('Status'),
+            getter: () => 'Unknown',
+          },
+          {
+            label: t('Age'),
+            getter: () => 'Unknown',
+          },
+        ]}
+        data={allowedNamespaces}
+      />
+    );
+  } else {
+    renderResource = (
       <ResourceTable
         resourceClass={Namespace}
         columns={[
@@ -29,6 +70,14 @@ export default function NamespacesList() {
           'age',
         ]}
       />
+    );
+  }
+
+  return (
+    <SectionBox
+      title={<SectionFilterHeader title={t('Namespaces')} noNamespaceFilter headerStyle="main" />}
+    >
+      {renderResource}
     </SectionBox>
   );
 }
