cfa46bedb939269fb9324e55ef259088591b0273
Joaquim Rocha <joaquim.rocha@microsoft.com>
2023-03-16 20:20:02 +0000

    app: Bump Chocolatey version to Headlamp 0.16.0

--- app/windows/chocolatey/tools/chocolateyinstall.ps1
+++ app/windows/chocolatey/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$headlampVersion = '0.15.0'
+$headlampVersion = '0.16.0'
 $url = "https://github.com/headlamp-k8s/headlamp/releases/download/v${headlampVersion}/Headlamp-${headlampVersion}-win-x64.exe"
-$checksum = '3dcee47552506f265b4b855f51f5e2d9286133c4d8beca9dad5022eccf03edf8'
+$checksum = '4adafcfc43132c1cb2113cf884b66ed3841252ea0d0f035dbe0d826d011db01e'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
