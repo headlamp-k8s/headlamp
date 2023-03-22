992c2ef99a08467384c6dc6910c798405dee8f02
Julian Li <jianzhili@microsft.com>
2023-03-22 17:25:38 +0800

    frontend: Make unit of request and limit in resource quota more readable

--- frontend/src/lib/k8s/resourceQuota.ts
+++ frontend/src/lib/k8s/resourceQuota.ts
@@ -1,3 +1,4 @@
+import { normalizeUnit } from '../util';
 import { apiFactoryWithNamespace } from './apiProxy';
 import { KubeObjectInterface, makeKubeObject } from './cluster';
 
@@ -42,33 +43,46 @@ class ResourceQuota extends makeKubeObject<KubeResourceQuota>('resourceQuota') {
 
   get requests(): string[] {
     const req: string[] = [];
-    Object.keys(this.spec.hard).forEach(key => {
-      if (key.startsWith('requests.')) {
-        req.push(`${key}: ${this.status.used[key]}/${this.spec.hard[key]}`);
-      }
-    });
+    this.spec.hard &&
+      Object.keys(this.spec.hard).forEach(key => {
+        if (key === 'cpu' || key === 'memory' || key.startsWith('requests.')) {
+          req.push(
+            `${key}: ${normalizeUnit(key, this.status.used[key])}/${normalizeUnit(
+              key,
+              this.spec.hard[key]
+            )}`
+          );
+        }
+      });
     return req;
   }
 
   get limits(): string[] {
     const limits: string[] = [];
-    Object.keys(this.spec.hard).forEach(key => {
-      if (key.startsWith('limits.')) {
-        limits.push(`${key}: ${this.status.used[key]}/${this.spec.hard[key]}`);
-      }
-    });
+    this.spec.hard &&
+      Object.keys(this.spec.hard).forEach(key => {
+        if (key.startsWith('limits.')) {
+          limits.push(
+            `${key}: ${normalizeUnit(key, this.status.used[key])}/${normalizeUnit(
+              key,
+              this.spec.hard[key]
+            )}`
+          );
+        }
+      });
     return limits;
   }
 
   get resourceStats() {
     const stats: { name: string; hard: string; used: string }[] = [];
-    Object.keys(this.status.hard).forEach(key => {
-      stats.push({
-        name: key,
-        hard: this.status.hard[key],
-        used: this.status.used[key],
+    this.status.hard &&
+      Object.keys(this.status.hard).forEach(key => {
+        stats.push({
+          name: key,
+          hard: `${this.status.hard[key]}`,
+          used: `${this.status.used[key]}`,
+        });
       });
-    });
     return stats;
   }
 }
