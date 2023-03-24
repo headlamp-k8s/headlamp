9da64a0c1a13e251b21e8b5f0aa2f2c810b639fe
René Dudfield <renedudfield@microsoft.com>
2023-03-24 05:40:46 +0100

    frontend Settings: Organize SettingsCluster and SettingsClusters
    Moved from one big file into separate files named after their
    components to follow convention.
    
    Mainly renamed from ClusterSettings because that aliased an
    existing type, and the new name matches the URL.

--- frontend/src/components/App/Settings/index.tsx
+++ frontend/src/components/App/Settings/index.tsx
@@ -1,5 +1,7 @@
 import Settings from './Settings';
 import SettingsButton from './SettingsButton';
+import SettingsCluster from './SettingsCluster';
+import SettingsClusters from './SettingsClusters';
 export * from './Settings';
 export default Settings;
-export { SettingsButton };
+export { SettingsButton, SettingsClusters, SettingsCluster };
