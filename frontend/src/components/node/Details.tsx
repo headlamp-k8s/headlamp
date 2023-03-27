723e59e01aae81294c7791502585fc1254a8daa4
ashu8912 <aghildiyal@microsoft.com>
2023-03-27 17:55:23 +0530

    frontend: Add cordon and uncordon option for node
    We want to prevent scheduling and unscheduling of more pods on a node
    and that's why in this patch we add the cordon and uncordon option in
    node details view.

--- frontend/src/components/node/Details.tsx
+++ frontend/src/components/node/Details.tsx
@@ -1,23 +1,41 @@
-import { InlineIcon } from '@iconify/react';
+import { Icon, InlineIcon } from '@iconify/react';
+import { IconButton, Tooltip } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import _ from 'lodash';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { apply } from '../../lib/k8s/apiProxy';
 import { KubeMetrics } from '../../lib/k8s/cluster';
 import Node from '../../lib/k8s/node';
 import { timeAgo } from '../../lib/util';
+import { clusterAction } from '../../redux/actions/actions';
 import { CpuCircularChart, MemoryCircularChart } from '../cluster/Charts';
 import { StatusLabelProps } from '../common';
 import { HeaderLabel, StatusLabel, ValueLabel } from '../common/Label';
 import { DetailsGrid, OwnedPodsSection } from '../common/Resource';
+import AuthVisible from '../common/Resource/AuthVisible';
 import { SectionBox } from '../common/SectionBox';
 import { NameValueTable } from '../common/SimpleTable';
 import DetailsViewSection from '../DetailsViewSection';
 
+function NodeConditionsLabel(props: { node: Node }) {
+  const { node } = props;
+  const unschedulable = node?.jsonData?.spec?.unschedulable;
+  const { t } = useTranslation();
+  return unschedulable ? (
+    <StatusLabel status="warning">{t('frequent|Scheduling Disabled')}</StatusLabel>
+  ) : (
+    <StatusLabel status="success">{t('frequent|Scheduling Enabled')}</StatusLabel>
+  );
+}
+
 export default function NodeDetails() {
   const { name } = useParams<{ name: string }>();
   const { t } = useTranslation('glossary');
+  const dispatch = useDispatch();
 
   const [nodeMetrics, metricsError] = Node.useMetrics();
 
@@ -32,6 +50,27 @@ export default function NodeDetails() {
     });
   }
 
+  function handleNodeScheduleState(node: Node, cordon: boolean) {
+    const cloneNode = _.cloneDeep(node);
+
+    cloneNode.spec.unschedulable = !cordon;
+    dispatch(
+      clusterAction(() => apply(cloneNode.jsonData), {
+        startMessage: cordon
+          ? t('Uncordoning node {{name}}...', {
+              name: node.metadata.name,
+            })
+          : t('Cordoning node {{name}}…', { name: node.metadata.name }),
+        successMessage: cordon
+          ? t('Uncordoned node {{name}}.', { name: node.metadata.name })
+          : t('Cordoned node {{name}}.', { name: node.metadata.name }),
+        errorMessage: cordon
+          ? t('Failed to uncordon node {{name}}.', { name: node.metadata.name })
+          : t('Failed to cordon node {{name}}.', { name: node.metadata.name }),
+      })
+    );
+  }
+
   return (
     <DetailsGrid
       headerSection={item => (
@@ -40,11 +79,31 @@ export default function NodeDetails() {
       resourceType={Node}
       name={name}
       withEvents
+      actions={item => {
+        const cordon = item?.jsonData?.spec?.unschedulable;
+        const cordonOrUncordon = cordon ? t('Uncordon') : t('Cordon');
+        return [
+          <AuthVisible item={item} authVerb="update">
+            <Tooltip title={cordonOrUncordon as string}>
+              <IconButton
+                aria-label={cordonOrUncordon}
+                onClick={() => handleNodeScheduleState(item, cordon)}
+              >
+                {cordon ? <Icon icon="mdi:check-circle-outline" /> : <Icon icon="mdi:cancel" />}
+              </IconButton>
+            </Tooltip>
+          </AuthVisible>,
+        ];
+      }}
       extraInfo={item =>
         item && [
           {
             name: t('frequent|Ready'),
             value: <NodeReadyLabel node={item} />,
+          },
+          {
+            name: t('frequent|Conditions'),
+            value: <NodeConditionsLabel node={item} />,
           },
           {
             name: t('Pod CIDR'),
