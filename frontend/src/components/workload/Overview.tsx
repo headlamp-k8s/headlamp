ecb3fb6453f7d7650e6244b06659a69636034458
Joaquim Rocha <joaquim.rocha@microsoft.com>
2023-03-30 15:40:39 +0100

    frontend: Improve workload/Overview on multi-cluster

--- frontend/src/components/workload/Overview.tsx
+++ frontend/src/components/workload/Overview.tsx
@@ -2,14 +2,22 @@ import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
-import { Workload } from '../../lib/k8s/cluster';
+import { ApiError } from '../../lib/k8s/apiProxy';
+import { KubeObject, Workload } from '../../lib/k8s/cluster';
 import CronJob from '../../lib/k8s/cronJob';
 import DaemonSet from '../../lib/k8s/daemonSet';
 import Deployment from '../../lib/k8s/deployment';
 import Job from '../../lib/k8s/job';
 import ReplicaSet from '../../lib/k8s/replicaSet';
 import StatefulSet from '../../lib/k8s/statefulSet';
-import { getReadyReplicas, getTotalReplicas, useFilterFunc } from '../../lib/util';
+import {
+  combineClusterListErrors,
+  flattenClusterListItems,
+  getReadyReplicas,
+  getTotalReplicas,
+  useFilterFunc,
+} from '../../lib/util';
+import { ClusterGroupErrorMessage } from '../cluster/ClusterGroupErrorMessage';
 import { PageGrid, ResourceLink } from '../common/Resource';
 import ResourceTable from '../common/Resource/ResourceTable';
 import { SectionBox } from '../common/SectionBox';
@@ -20,21 +28,40 @@ interface WorkloadDict {
   [key: string]: Workload[];
 }
 
+function useWorkloadList(
+  c: KubeObject,
+  setWorkloadsFunc: React.Dispatch<React.SetStateAction<WorkloadDict>>,
+  setErrorsPerCluster: React.Dispatch<
+    React.SetStateAction<{ [cluster: string]: ApiError | null } | null>
+  >
+) {
+  const [items, errorsPerCluster] = c.useListPerCluster();
+
+  if (!!errorsPerCluster && Object.keys(errorsPerCluster).length > 0) {
+    console.debug(`Error getting ${c.className} workloads: `, errorsPerCluster);
+  }
+
+  React.useEffect(() => {
+    if (items !== null) {
+      setWorkloadsFunc(workloads => {
+        return { ...workloads, [c.className]: flattenClusterListItems(items) };
+      });
+    }
+    if (errorsPerCluster !== null) {
+      // We use joinClusterListErrors here so it discards the null values.
+      setErrorsPerCluster(errors => combineClusterListErrors(errors, errorsPerCluster));
+    }
+  }, [items, errorsPerCluster]);
+}
+
 export default function Overview() {
-  const [workloadsData, dispatch] = React.useReducer(setWorkloads, {});
+  const [workloadsData, setWorkloadsData] = React.useState<WorkloadDict>({});
+  const [errorsPerCluster, setErrorsPerCluster] = React.useState<{
+    [cluster: string]: ApiError | null;
+  } | null>(null);
   const location = useLocation();
   const filterFunc = useFilterFunc(['.jsonData.kind']);
   const { t } = useTranslation('glossary');
-
-  function setWorkloads(
-    workloads: WorkloadDict,
-    { items, kind }: { items: Workload[]; kind: string }
-  ) {
-    const data = { ...workloads };
-    data[kind] = items;
-
-    return data;
-  }
 
   function getPods(item: Workload) {
     return `${getReadyReplicas(item)}/${getTotalReplicas(item)}`;
@@ -51,21 +78,30 @@ export default function Overview() {
 
   function getJointItems() {
     let joint: Workload[] = [];
+    let hasItems = false;
     for (const items of Object.values(workloadsData)) {
-      joint = joint.concat(items);
+      if (!items) {
+        continue;
+      }
+      hasItems = true;
+      joint = joint.concat(items.filter(i => i !== null));
+    }
+    if (!hasItems) {
+      return null;
     }
     return joint;
   }
 
   const workloads = [DaemonSet, Deployment, Job, CronJob, ReplicaSet, StatefulSet];
-  workloads.forEach(workloadClass => {
-    workloadClass.useApiList((items: InstanceType<typeof workloadClass>[]) =>
-      dispatch({ items, kind: workloadClass.className })
-    );
-  });
+  for (const c of workloads) {
+    useWorkloadList(c, setWorkloadsData, setErrorsPerCluster);
+  }
 
   return (
     <PageGrid>
+      {Object.keys(errorsPerCluster || {}).length > 0 && (
+        <ClusterGroupErrorMessage clusters={errorsPerCluster!} />
+      )}
       <SectionBox py={2}>
         <Grid container justifyContent="space-around" alignItems="flex-start" spacing={1}>
           {workloads.map(({ className: name }) => (
@@ -101,6 +137,7 @@ export default function Overview() {
               },
             },
             'namespace',
+            'cluster',
             {
               label: t('Pods'),
               getter: item => item && getPods(item),
