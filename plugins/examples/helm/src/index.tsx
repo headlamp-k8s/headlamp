b8ba19b15749a21873f8417319ddc56a89cf2b64
ashu8912 <aghildiyal@microsoft.com>
2023-04-03 10:34:02 +0530

    plugin: Init helm plugin

---
+++ plugins/examples/helm/src/index.tsx
@@ -0,0 +1,9 @@
+import { registerAppBarAction } from '@kinvolk/headlamp-plugin/lib';
+
+// Below are some imports you may want to use.
+//   See README.md for links to plugin development documentation.
+// import { SectionBox } from '@kinvolk/headlamp-plugin/lib/CommonComponents';
+// import { K8s } from '@kinvolk/headlamp-plugin/lib/K8s';
+// import { Typography } from '@material-ui/core';
+
+registerAppBarAction(<span>Hello</span>);
