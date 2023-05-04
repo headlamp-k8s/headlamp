a3c7afe355514c7fab7fe905cb307cdf7c78048a
Joaquim Rocha <joaquim.rocha@microsoft.com>
2023-05-04 14:22:39 +0100

    frontend: Go back to the cluster selection on error in KubeConfigLoader
    If a cluster is selected to be added but it fails due to be set up in
    the backend, then the user would be stuck in a dialog with an endless
    spinner and an error message.
    This patch goes back to the cluster selection on such errors, meaning
    the user has a choice to proceed from there.

--- frontend/src/components/cluster/KubeConfigLoader.tsx
+++ frontend/src/components/cluster/KubeConfigLoader.tsx
@@ -175,8 +175,10 @@ function KubeConfigLoader() {
               setState(Step.Success);
             }
           })
-          .catch(() => {
+          .catch(e => {
+            console.debug('Error setting up clusters from kubeconfig:', e);
             setError(t('cluster|Error setting up clusters, please load a valid kubeconfig file'));
+            setState(Step.SelectClusters);
           });
       }
       loadClusters();
