6a96bb81d107cad13ac1d710adbaab29b432f7a6
yolossn <sannagaraj@microsoft.com>
2023-05-06 13:38:07 +0530

    backend: Add caching package
    The helm package uses a cache to store results
    of async functions. Since similar in-memory store
    is required in multiple places a separate cache
    package is created.
    
    Signed-off-by: yolossn <sannagaraj@microsoft.com>

---
+++ backend/pkg/cache/cache_test.go
@@ -0,0 +1,51 @@
+package cache_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/headlamp-k8s/headlamp/backend/pkg/cache"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testCache(ch cache.Cache, t *testing.T) {
+	t.Helper()
+
+	// set value
+	err := ch.Set(context.Background(), "key1", "value1")
+	require.NoError(t, err)
+
+	// get value
+	value, err := ch.Get(context.Background(), "key1")
+	assert.NoError(t, err)
+	assert.Equal(t, "value1", value)
+
+	// get a value that doesn't exists
+	value, err = ch.Get(context.Background(), "key2")
+	assert.Error(t, err)
+	assert.Nil(t, value)
+
+	// delete value
+	err = ch.Delete(context.Background(), "key1")
+	assert.NoError(t, err)
+
+	// set value with ttl
+	err = ch.SetWithTTL(context.Background(), "ttlkey1", "value1", time.Second)
+	require.NoError(t, err)
+
+	time.Sleep(3 * time.Second)
+
+	// get value with ttl
+	value, err = ch.Get(context.Background(), "ttlkey1")
+	assert.Equal(t, cache.ErrNotFound, err)
+	assert.Error(t, err)
+	assert.Nil(t, value)
+}
+
+func TestCache(t *testing.T) {
+	// create cache
+	ch := cache.New()
+	testCache(ch, t)
+}
