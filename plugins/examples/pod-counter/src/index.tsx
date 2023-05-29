3320f6e29099849c5521bd67c2bc46923d834b9e
René Dudfield <renedudfield@microsoft.com>
2023-05-29 09:40:13 +0200

    plugins/examples: Add app bar action button processor to pod-counter

--- plugins/examples/pod-counter/src/index.tsx
+++ plugins/examples/pod-counter/src/index.tsx
@@ -1,4 +1,4 @@
-import { K8s, registerAppBarAction } from '@kinvolk/headlamp-plugin/lib';
+import { DefaultAppBarAction, K8s, registerAppBarAction } from '@kinvolk/headlamp-plugin/lib';
 import Message from './Message';
 
 function PodCounter() {
@@ -9,11 +9,19 @@ function PodCounter() {
 
 registerAppBarAction(PodCounter);
 
-// registerAppBarAction({
-//   processor: (_, actions) => {
-//     const newActions = actions.filter(action => action.id !== 'pod-counter')
-//     newActions.push({action: <PodCounter />, id: 'pod-counter'})
-//     return newActions;
-//   },
-//   id: 'pod-counter-processor',
-// });
+// We can also reorder the actions in the app bar.
+registerAppBarAction((_, actions) => {
+  // Remove the notifications action button
+  const newActions = actions.filter(action => action.id !== DefaultAppBarAction.NOTIFICATION);
+
+  // Add an extra pod counter action button
+  // newActions.push({action: <PodCounter />, id: 'pod-counter
+
+  // Move the notification action to the end.
+  const notificationAction = actions.filter(
+    action => action.id === DefaultAppBarAction.NOTIFICATION
+  )[0];
+  newActions.push(notificationAction);
+
+  return newActions;
+});
