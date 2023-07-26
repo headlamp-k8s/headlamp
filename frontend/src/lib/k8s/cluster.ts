9e650ce16af2af26952bcfe13a8ad1f40cab72b5
Joaquim Rocha <joaquim.rocha@microsoft.com>
2023-07-26 19:09:10 +0100

    frontend cluster.ts: Add missing generation entry in KubeMetadata

--- frontend/src/lib/k8s/cluster.ts
+++ frontend/src/lib/k8s/cluster.ts
@@ -55,6 +55,7 @@ export interface KubeMetadata {
   annotations?: StringDict;
   ownerReferences?: KubeOwnerReference[];
   managedFields?: KubeManagedFields[];
+  generation?: number;
 }
 
 export interface KubeOwnerReference {
