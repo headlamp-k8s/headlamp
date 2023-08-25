684dd98ea46adc3163459eb3b61624efffed68f8
Vincent T <vtaylor@microsoft.com>
2023-08-25 09:38:37 -0400

    windows: Update chocolatey to 0.19.1

--- app/windows/chocolatey/tools/chocolateyinstall.ps1
+++ app/windows/chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$headlampVersion = '0.19.0'
+$headlampVersion = '0.19.1'
 $url = "https://github.com/headlamp-k8s/headlamp/releases/download/v${headlampVersion}/Headlamp-${headlampVersion}-win-x64.exe"
 $checksum = '3651bb6649b5bd59525c9e4ac6dc418813a6a02e4d5cedf2b46219a8fe3b0e27'
 
