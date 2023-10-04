0f46897ea91364ab9d9e78af3cc8b0185c7432c4
Joaquim Rocha <joaquim.rocha@microsoft.com>
2023-10-04 14:54:07 +0100

    frontend PersistentVolumeClaim: Make its limits field optional
    As it's not mandatory.
    
    Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

---
+++ frontend/src/components/storage/ClaimList.stories.tsx
@@ -0,0 +1,70 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import _ from 'lodash';
+import PersistentVolumeClaim, {
+  KubePersistentVolumeClaim,
+} from '../../lib/k8s/persistentVolumeClaim';
+import { TestContext } from '../../test';
+import PVClaimList from './ClaimList';
+
+const basePVC = {
+  apiVersion: 'v1',
+  kind: 'PersistentVolumeClaim',
+  metadata: {
+    creationTimestamp: '2023-04-27T20:31:27Z',
+    finalizers: ['kubernetes.io/pvc-protection'],
+    name: 'my-pvc',
+    namespace: 'default',
+    resourceVersion: '1234',
+    uid: 'abc-1234',
+  },
+  spec: {
+    accessModes: ['ReadWriteOnce'],
+    resources: {
+      requests: {
+        storage: '8Gi',
+      },
+    },
+    storageClassName: 'default',
+    volumeMode: 'Filesystem',
+    volumeName: 'pvc-abc-1234',
+  },
+  status: {
+    accessModes: ['ReadWriteOnce'],
+    capacity: {
+      storage: '8Gi',
+    },
+    phase: 'Bound',
+  },
+};
+
+PersistentVolumeClaim.useList = () => {
+  const noStorageClassNamePVC = _.cloneDeep(basePVC);
+  noStorageClassNamePVC.metadata.name = 'no-storage-class-name-pvc';
+  noStorageClassNamePVC.spec.storageClassName = '';
+
+  const objList = [basePVC, noStorageClassNamePVC].map(
+    pvc => new PersistentVolumeClaim(pvc as KubePersistentVolumeClaim)
+  );
+  return [objList, null, () => {}, () => {}] as any;
+};
+
+export default {
+  title: 'Storage/PersistentVolumeClaim',
+  component: PVClaimList,
+  argTypes: {},
+  decorators: [
+    Story => {
+      return (
+        <TestContext>
+          <Story />
+        </TestContext>
+      );
+    },
+  ],
+} as Meta;
+
+const Template: Story = () => {
+  return <PVClaimList />;
+};
+
+export const ListView = Template.bind({});
