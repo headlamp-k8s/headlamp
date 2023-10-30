f88372358f80026db835be104acc713f2acdf532
Vincent T <vtaylor@microsoft.com>
2023-10-30 11:19:25 -0400

    frontend: Fix endpoint address field CSS
    Signed-off-by: Vincent T <vtaylor@microsoft.com>

--- frontend/src/components/service/Details.tsx
+++ frontend/src/components/service/Details.tsx
@@ -101,7 +101,6 @@ export default function ServiceDetails() {
                       {
                         label: t('translation|Addresses'),
                         getter: endpoint => endpoint.getAddressesText(),
-                        cellProps: { style: { width: '40%', maxWidth: '40%' } },
                       },
                     ]}
                     reflectInURL="endpoints"
