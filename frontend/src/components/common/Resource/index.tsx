e88cb9f203eb659a5868d573d026db9c896ef118
Jan Jansen <jan.jansen@gdata.de>
2023-12-15 17:26:24 +0100

    frontend: Export the Resource's ResourceListView and AuthVisible
    Signed-off-by: Jan Jansen <jan.jansen@gdata.de>

--- frontend/src/components/common/Resource/index.tsx
+++ frontend/src/components/common/Resource/index.tsx
@@ -2,3 +2,7 @@ export * from './CircularChart';
 export * from './MetadataDisplay';
 export * from './Resource';
 export * as ResourceTable from './ResourceTable';
+export * from './ResourceListView';
+export { default as ResourceListView } from './ResourceListView';
+export * from './AuthVisible';
+export { default as AuthVisible } from './AuthVisible';
