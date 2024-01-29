5a59cefb52b01eed8cf4b070e50c9b9af1846f14
yolossn <sannagaraj@microsoft.com>
2024-01-30 00:12:14 +0530

    frontend: Add PluginDetails page
    this commit adds PluginDetails page and
    storybook for it.
    
    Signed-off-by: yolossn <sannagaraj@microsoft.com>

--- frontend/src/components/App/PluginSettings/PluginSettings.tsx
+++ frontend/src/components/App/PluginSettings/PluginSettings.tsx
@@ -8,7 +8,7 @@ import { useDispatch } from 'react-redux';
 import { useFilterFunc } from '../../../lib/util';
 import { PluginInfo, reloadPage, setPluginSettings } from '../../../plugin/pluginsSlice';
 import { useTypedSelector } from '../../../redux/reducers/reducers';
-import { SectionBox, SimpleTable } from '../../common';
+import { Link as HeadlampLink, SectionBox, SimpleTable } from '../../common';
 import SectionFilterHeader from '../../common/SectionFilterHeader';
 
 /**
@@ -170,7 +170,15 @@ export function PluginSettingsPure(props: PluginSettingsPureProps) {
               getter: plugin => {
                 return (
                   <>
-                    <Typography variant="subtitle1">{plugin.name}</Typography>
+                    <Typography variant="subtitle1">
+                      <HeadlampLink
+                        routeName={'pluginDetails'}
+                        params={{ name: plugin.name }}
+                        align="right"
+                      >
+                        {plugin.name}
+                      </HeadlampLink>
+                    </Typography>
                     <Typography variant="caption">{plugin.version}</Typography>
                   </>
                 );
