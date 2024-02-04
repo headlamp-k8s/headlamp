5982533b31acf6071d0ccd8374b038829e0cb989
Mariusz Winnik <tazo90@gmail.com>
2024-02-04 01:20:05 +0100

    Add vite config
    Signed-off-by: Mariusz Winnik <tazo90@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,30 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
+export default defineConfig({
+  build: {
+    outDir: 'build',
+    commonjsOptions: { transformMixedEsModules: true },
+  },
+  plugins: [svgr(), react()],
+  define: {
+    // By default, Vite doesn't include shims for NodeJS
+    // necessary for @octokit_core lib to work
+    global: {},
+    'process.env': {},
+  },
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      buffer: 'buffer',
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      http: 'stream-http',
+      https: 'https-browserify',
+      os: 'os-browserify',
+      url: 'url',
+      util: 'util',
     },
-    plugins: [svgr(), react()],
-  };
+  },
 });
