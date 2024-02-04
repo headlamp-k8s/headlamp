e33e1f8d7e4979a9572a155925ee903205db29ae
Mariusz Winnik <tazo90@gmail.com>
2024-02-04 01:47:39 +0100

    Setup jest as test runner
    Signed-off-by: Mariusz Winnik <tazo90@gmail.com>

---
+++ frontend/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    // process `*.tsx` files with `ts-jest`
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+  },
+};
