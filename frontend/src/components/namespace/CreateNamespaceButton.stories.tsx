41d7c90717f058af61c32d40e185cf09f454ffc8
Vincent T <vtaylor@microsoft.com>
2024-02-14 15:15:28 -0500

    frontend: Add create namespace ui
    Signed-off-by: Vincent T <vtaylor@microsoft.com>

---
+++ frontend/src/components/namespace/CreateNamespaceButton.stories.tsx
@@ -0,0 +1,94 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { expect, userEvent, waitFor } from '@storybook/test';
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { TestContext } from '../../test';
+import CreateNamespaceButton from './CreateNamespaceButton';
+
+const meta: Meta<typeof CreateNamespaceButton> = {
+  title: 'Namespace/CreateNamespaceButton',
+  component: CreateNamespaceButton,
+  decorators: [
+    Story => {
+      return (
+        <TestContext>
+          <Story />
+        </TestContext>
+      );
+    },
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof CreateNamespaceButton>;
+
+export const OkayName: Story = {
+  play: async () => {
+    await userEvent.click(screen.getByLabelText('Create'));
+
+    await waitFor(() => expect(screen.getByLabelText('Dialog')).toBeVisible());
+
+    await waitFor(() => userEvent.type(screen.getByRole('textbox'), 'okay-name'), {
+      timeout: 5000,
+    });
+
+    const button = await screen.findByRole('button', { name: 'Create' });
+
+    expect(button).toBeEnabled();
+  },
+};
+
+export const EmptyName: Story = {
+  play: async () => {
+    await userEvent.click(screen.getByLabelText('Create'));
+
+    await waitFor(() => expect(screen.getByLabelText('Dialog')).toBeVisible());
+
+    await waitFor(() => userEvent.type(screen.getByRole('textbox'), ' '), { timeout: 5000 });
+
+    const button = await screen.findByRole('button', { name: 'Create' });
+    const errorMessage = await screen.findByText(
+      "Namespaces must contain only lowercase alphanumeric characters or '-', and must start and end with an alphanumeric character."
+    );
+
+    expect(errorMessage).toBeVisible();
+    expect(button).not.toBeEnabled();
+  },
+};
+
+export const NotValidName: Story = {
+  play: async () => {
+    await userEvent.click(screen.getByLabelText('Create'));
+
+    await waitFor(() => expect(screen.getByLabelText('Dialog')).toBeVisible());
+
+    await waitFor(() => userEvent.type(screen.getByRole('textbox'), 'not-valid-name-'), {
+      timeout: 5000,
+    });
+
+    const button = await screen.findByRole('button', { name: 'Create' });
+    const errorMessage = await screen.findByText(
+      "Namespaces must contain only lowercase alphanumeric characters or '-', and must start and end with an alphanumeric character."
+    );
+
+    expect(errorMessage).toBeVisible();
+    expect(button).not.toBeEnabled();
+  },
+};
+
+export const NotValidNameLong: Story = {
+  play: async () => {
+    const longName = 'w'.repeat(64);
+    await userEvent.click(screen.getByLabelText('Create'));
+
+    await waitFor(() => expect(screen.getByLabelText('Dialog')).toBeVisible());
+
+    await waitFor(() => userEvent.type(screen.getByRole('textbox'), longName), { timeout: 10000 });
+
+    const button = await screen.findByRole('button', { name: 'Create' });
+    const errorMessage = await screen.findByText('Namespaces must be under 64 characters.');
+
+    expect(errorMessage).toBeVisible();
+    expect(button).not.toBeEnabled();
+  },
+};
--- frontend/src/lib/k8s/namespace.ts
+++ frontend/src/lib/k8s/namespace.ts
@@ -13,6 +13,24 @@ class Namespace extends makeKubeObject<KubeNamespace>('namespace') {
   get status() {
     return this.jsonData!.status;
   }
+
+  /**
+   * This function validates the custom namespace input matches the crieria for DNS-1123 label names.
+   * @returns true if the namespace is valid, false otherwise.
+   * @params namespace: string
+   * @see https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#dns-label-names
+   */
+  static isValidNamespaceFormat(namespace: string) {
+    // Validates that the namespace is under 64 characters.
+    if (namespace.length > 63) {
+      return false;
+    }
+
+    // Validates that the namespace contains only lowercase alphanumeric characters or '-',
+    const regex = new RegExp('^[a-z0-9]([-a-z0-9]*[a-z0-9])?$');
+
+    return regex.test(namespace);
+  }
 }
 
 export default Namespace;
