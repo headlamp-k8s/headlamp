8d6ca506b038c87d64d4b4c320066d560237113c
René Dudfield <renedudfield@microsoft.com>
2024-03-06 17:10:43 +0100

    e2e-tests: headlamp.spec: Add test for pagination
    So we can see it updates which page it is on.
    
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

--- e2e-tests/tests/headlamp.spec.ts
+++ e2e-tests/tests/headlamp.spec.ts
@@ -1,35 +1,35 @@
-import { test, expect } from '@playwright/test';
-import { HeadlampPage } from './headlampPage';
-import { ServicesPage } from './servicesPage';
-import { SecurityPage } from './securityPage';
+import { test, expect } from "@playwright/test";
+import { HeadlampPage } from "./headlampPage";
+import { ServicesPage } from "./servicesPage";
+import { SecurityPage } from "./securityPage";
 
 // --- Plugins tests start --- //
-test('GET /plugins/list returns plugins list', async ({ page }) => {
-  const response: any = await page.goto('/plugins');
+test("GET /plugins/list returns plugins list", async ({ page }) => {
+  const response: any = await page.goto("/plugins");
   expect(response).toBeTruthy();
 
   const json = await response.json();
   expect(json.length).toBeGreaterThan(0);
-  expect(json.some(str => str.includes('plugins/'))).toBeTruthy();
+  expect(json.some((str) => str.includes("plugins/"))).toBeTruthy();
 });
 // --- Plugin tests end --- //
 
 // --- Headlamp tests start --- //
-test('headlamp is there and so is minikube', async ({ page }) => {
+test("headlamp is there and so is minikube", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
 
   await headlampPage.authenticate();
   await headlampPage.hasURLContaining(/.*main/);
 });
 
-test('main page should have Network tab', async ({ page }) => {
+test("main page should have Network tab", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
 
   await headlampPage.authenticate();
   await headlampPage.hasNetworkTab();
 });
 
-test('service page should have headlamp service', async ({ page }) => {
+test("service page should have headlamp service", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
   const servicesPage = new ServicesPage(page);
 
@@ -38,10 +38,10 @@ test('service page should have headlamp service', async ({ page }) => {
   await servicesPage.clickOnServicesSection();
 
   // Check if there is text "headlamp" on the page
-  await headlampPage.checkPageContent('headlamp');
+  await headlampPage.checkPageContent("headlamp");
 });
 
-test('headlamp service page should contain port', async ({ page }) => {
+test("headlamp service page should contain port", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
   const servicesPage = new ServicesPage(page);
 
@@ -51,17 +51,17 @@ test('headlamp service page should contain port', async ({ page }) => {
   await servicesPage.goToParticularService("headlamp");
 
   // Check if there is text "TCP" on the page
-  await headlampPage.checkPageContent('TCP');
+  await headlampPage.checkPageContent("TCP");
 });
 
-test('main page should have Security tab', async ({ page }) => {
+test("main page should have Security tab", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
 
   await headlampPage.authenticate();
   await headlampPage.hasSecurityTab();
 });
 
-test('Service account tab should have headlamp-admin', async ({ page }) => {
+test("Service account tab should have headlamp-admin", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
   const securityPage = new SecurityPage(page);
 
@@ -70,20 +70,46 @@ test('Service account tab should have headlamp-admin', async ({ page }) => {
   await securityPage.clickOnServiceAccountsSection();
 
   // Check if there is text "headlamp-admin" on the page
-  await headlampPage.checkPageContent('headlamp-admin');
+  await headlampPage.checkPageContent("headlamp-admin");
 });
 
-test('Logout the user', async ({ page }) => {
+test("Logout the user", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
 
   await headlampPage.authenticate();
   await headlampPage.logout();
 });
 
-test('404 page is present', async ({ page }) => {
+test("404 page is present", async ({ page }) => {
   const headlampPage = new HeadlampPage(page);
 
   await headlampPage.authenticate();
-  await headlampPage.navigateTopage('/404test', /Whoops! This page doesn't exist/);
+  await headlampPage.navigateTopage(
+    "/404test",
+    /Whoops! This page doesn't exist/
+  );
 });
+
+test("pagination goes to next page", async ({ page }) => {
+  const headlampPage = new HeadlampPage(page);
+  await headlampPage.authenticate();
+
+  await page.click('span:has-text("Security")');
+  await page.waitForLoadState("load");
+  await page.waitForSelector('span:has-text("Service Accounts")');
+
+  await page.click('span:has-text("Roles")');
+  await page.waitForSelector('span:has-text("Roles")');
+  await page.waitForSelector('p:has-text("Rows per page")');
+
+  const paginationCaption = page.locator("p:has-text(' of ')");
+  const captionText = await paginationCaption.textContent();
+
+  const nextPageButton = page.getByTitle("Next page");
+  await nextPageButton.click();
+  const captionText2 = await paginationCaption.textContent();
+
+  expect(captionText).not.toBe(captionText2);
+});
+
 // --- Headlamp tests end --- //
