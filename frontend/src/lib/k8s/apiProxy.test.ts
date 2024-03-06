96db762fb5f97cc06f4217676401be12932520ca
Evangelos Skopelitis <eskopelitis@microsoft.com>
2024-03-06 17:25:13 -0500

    Added tests for request and apiFactory
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- frontend/src/lib/k8s/apiProxy.test.ts
+++ frontend/src/lib/k8s/apiProxy.test.ts
@@ -1,22 +1,62 @@
 import nock from 'nock';
 import * as apiProxy from './apiProxy';
 
-describe('apiProxy functions', () => {
-  const baseApiUrl = 'http://localhost';
+const baseApiUrl = 'http://localhost';
+const mockResponse = { data: 'mock response' };
+const clusterName = 'test-cluster';
+
+// describe('apiFactory', () => {
+//   const mockSingleResource = ['groupA', 'v1', 'resourceA'];
+//   const mockMultipleResource = [['groupB', 'v1', 'resourceB'], ['groupC', 'v1', 'resourceC']];
+
+//   beforeEach(() => {
+//     nock.disableNetConnect();
+//     nock(baseApiUrl)
+//       // Successful GET request on single resource
+//       .persist()
+//       .get(`/clusters/${clusterName}/apis/${mockSingleResource[0]}/${mockSingleResource[1]}/${mockSingleResource[2]}`)
+//       .reply(200, mockResponse)
+//       // Successful GET request on multiple resources
+//       .persist()
+//       .get(`/clusters/${clusterName}/apis/${mockMultipleResource[0][0]}/${mockMultipleResource[0][1]}/${mockMultipleResource[0][2]}`)
+//       .reply(200, mockResponse)
+//       .persist()
+//       .get(`/clusters/${clusterName}/apis/${mockMultipleResource[1][0]}/${mockMultipleResource[1][1]}/${mockMultipleResource[1][2]}`)
+//       .reply(200, mockResponse);
+//   });
+
+//   afterEach(() => {
+//     nock.cleanAll();
+//     nock.enableNetConnect();
+//   });
+
+//   it('Successfully creates API client for single resource', async () => {
+//     const client = apiProxy.apiFactory(mockSingleResource[0], mockSingleResource[1], mockSingleResource[2]);
+//     const cb = jest.fn();
+//     const errCb = jest.fn();
+
+//     await client.list(cb, errCb, {}, clusterName);
+//     expect(cb).toHaveBeenCalledWith(mockResponse);
+//   });
+// });
+
+describe('get, post, patch, put, delete', () => {
   const testPath = '/test/url';
-  const clusterName = 'test-cluster';
   const testData = { key: 'value' };
-  const mockResponse = { data: 'mock response' };
-  const errorResponse401 = { error: 'Unauthorized' };
-  const errorResponse500 = { error: 'Internal Server Error' };
+  const errorResponse401 = { error: 'Unauthorized', message: 'Unauthorized' };
+  const errorResponse500 = { error: 'Internal Server Error', message: 'Unauthorized' };
 
   beforeAll(() => {
     nock.disableNetConnect();
   });
 
   beforeEach(() => {
-    // Successful POST request
     nock(baseApiUrl)
+      // Successful GET request
+      .persist()
+      .get(`/clusters/${clusterName}` + testPath)
+      .reply(200, mockResponse)
+      // Successful POST request
       .persist()
       .post(`/clusters/${clusterName}` + testPath, testData)
       .reply(200, mockResponse)
@@ -42,19 +82,39 @@ describe('apiProxy functions', () => {
     nock.enableNetConnect();
   });
 
-  it('Correctly calls post', async () => {
+  it('Successfully calls request', async () => {
+    const response = await apiProxy.request(`/clusters/${clusterName}` + testPath, {}, true, true);
+    expect(response).toEqual(mockResponse);
+  });
+
+  it.each([
+    [401, errorResponse401],
+    [500, errorResponse500],
+  ])('Successfully handles request with error status %d', async (statusCode, errorResponse) => {
+    nock.cleanAll();
+    nock(baseApiUrl)
+      .persist()
+      .get(`/clusters/${clusterName}` + testPath)
+      .reply(statusCode, { message: errorResponse.error });
+
+    await expect(
+      apiProxy.request(`/clusters/${clusterName}` + testPath, {}, true, true)
+    ).rejects.toThrow(`${errorResponse.error}`);
+  });
+
+  it('Successfully calls post', async () => {
     const response = await apiProxy.post(testPath, testData, true, { cluster: clusterName });
     expect(response).toEqual(mockResponse);
   });
-  it('Correctly calls patch', async () => {
+  it('Successfully calls patch', async () => {
     const response = await apiProxy.patch(testPath, testData, true, { cluster: clusterName });
     expect(response).toEqual(mockResponse);
   });
-  it('Correctly calls put', async () => {
+  it('Successfully calls put', async () => {
     const response = await apiProxy.put(testPath, testData, true, { cluster: clusterName });
     expect(response).toEqual(mockResponse);
   });
-  it('Correctly calls remove', async () => {
+  it('Successfully calls remove', async () => {
     const response = await apiProxy.remove(testPath, { cluster: clusterName });
     expect(response).toEqual(mockResponse);
   });
@@ -69,7 +129,7 @@ describe('apiProxy functions', () => {
     ['put', 500, errorResponse500],
     ['remove', 401, errorResponse401],
     ['remove', 500, errorResponse500],
-  ])('Correctly handles %s with status %d', async (method, statusCode, errorResponse) => {
+  ])('Successfully handles %s with status %d', async (method, statusCode, errorResponse) => {
     nock.cleanAll();
     const scope = nock(baseApiUrl).persist();
     let promise;
