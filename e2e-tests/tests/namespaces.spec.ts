b6eb3fa0b48dacb6c8876dc38669177c3c7cdaa7
René Dudfield <renedudfield@microsoft.com>
2024-03-11 20:12:18 +0100

    e2e-tests: namespaces: Add test for creating and deleting
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

---
+++ e2e-tests/tests/namespaces.spec.ts
@@ -0,0 +1,25 @@
+import { test } from "@playwright/test";
+import { HeadlampPage } from "./headlampPage";
+import { NamespacesPage } from "./namespacesPage";
+
+test("create a namespace with the minimal editor then delete it", async ({
+  page,
+}) => {
+  const name = "testing-e2e";
+  const headlampPage = new HeadlampPage(page);
+  await headlampPage.authenticate();
+
+  await page.click('span:has-text("Cluster")');
+  await page.waitForLoadState("load");
+
+  // If there's no namespaces permission, then we return
+  const content = await page.content();
+  if (!content.includes("Namespaces") || !content.includes('href="/c/main/namespaces')) {
+    return;
+  }
+
+  const namespacesPage = new NamespacesPage(page);
+  await namespacesPage.navigateToNamespaces();
+  await namespacesPage.createNamespace(name);
+  await namespacesPage.deleteNamespace(name);
+});
---
+++ e2e-tests/tests/namespacesPage.ts
@@ -0,0 +1,58 @@
+import { Page } from "@playwright/test";
+
+export class NamespacesPage {
+  constructor(private page: Page) {}
+
+  async navigateToNamespaces() {
+    await this.page.click('span:has-text("Cluster")');
+    await this.page.waitForLoadState("load");
+    await this.page.waitForSelector('span:has-text("Namespaces")');
+    await this.page.click('span:has-text("Namespaces")');
+  }
+
+  async createNamespace(name) {
+    const yaml = `
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: ${name}
+    `;
+    const page = this.page;
+
+    await page.waitForSelector('span:has-text("Namespaces")');
+
+    await page.click('span:has-text("Namespaces")');
+    await page.waitForLoadState("load");
+
+    // If the namespace already exists, return.
+    // This makes it a bit more resilient to flakiness.
+    const pageContent = await this.page.content();
+    if (pageContent.includes(name)) {
+      return
+    }
+
+    await page.click('button[title="Create / Apply"]');
+    await page.waitForLoadState("load");
+
+    await page.click('span:has-text("Use minimal editor")');
+    await page.waitForLoadState("load");
+
+    await page.fill('textarea[aria-label="yaml Code"]', yaml);
+    await page.click('button:has-text("Apply")');
+    await page.waitForSelector(`text=Applied ${name}`);
+  }
+
+  async deleteNamespace(name) {
+    const page = this.page;
+    await page.click('span:has-text("Namespaces")');
+    await page.waitForLoadState("load");
+    await page.waitForSelector(`text=${name}`);
+    await page.click(`a:has-text("${name}")`);
+    await page.click('button[title="Delete"]');
+    await page.waitForSelector(
+      "text=Are you sure you want to delete this item?"
+    );
+    await page.click('button:has-text("Yes")');
+    await page.waitForSelector(`text=Deleted item ${name}`);
+  }
+}
