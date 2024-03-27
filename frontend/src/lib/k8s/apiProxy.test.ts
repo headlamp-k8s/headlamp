fe50d9d82075a25e677bc8c126d0112a753689fb
Evangelos Skopelitis <eskopelitis@microsoft.com>
2024-03-27 13:56:47 -0400

    Added clusterRequest tests
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- frontend/src/lib/k8s/apiProxy.test.ts
+++ frontend/src/lib/k8s/apiProxy.test.ts
@@ -1,9 +1,11 @@
 import WS from 'jest-websocket-mock';
 import nock from 'nock';
+import * as auth from '../auth';
 import * as cluster from '../cluster';
 import * as apiProxy from './apiProxy';
 
 const baseApiUrl = 'http://localhost';
+const testPath = '/test/url';
 const mockResponse = { message: 'mock response' };
 const clusterName = 'test-cluster';
 const podName = 'test-pod';
@@ -55,7 +57,56 @@ const modifiedConfigMap = {
   data: { ...mockConfigMap.data, volumeName: 'pvc-xyz-5678' },
 };
 
-// describe('clusterRequest', () => {});
+describe('clusterRequest', () => {
+  beforeEach(() => {
+    nock.cleanAll();
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('Successfully makes a request', async () => {
+    nock(baseApiUrl).get(testPath).reply(200, mockResponse);
+
+    const response = await apiProxy.clusterRequest(testPath);
+    expect(response).toEqual(mockResponse);
+  });
+
+  it('Successfully handles clusterRequest with status 401', async () => {
+    nock(baseApiUrl).get(testPath).reply(401, errorResponse401);
+
+    expect.assertions(1);
+    try {
+      await expect(apiProxy.clusterRequest(testPath)).rejects.toThrow(`${errorResponse401.error}`);
+    } catch (error: any) {
+      expect(error.status).toEqual(401);
+    }
+  });
+
+  it('Successfully appends query parameters', async () => {
+    const queryParams = {
+      watch: '1',
+      param1: 'value1',
+      param2: 'value2',
+    };
+
+    nock(baseApiUrl).get(testPath).query(queryParams).reply(200, mockResponse);
+
+    const response = await apiProxy.clusterRequest(testPath, {}, queryParams);
+    expect(response).toEqual(mockResponse);
+  });
+
+  it('Successfully handles X-Authorization for token refresh', async () => {
+    nock(baseApiUrl)
+      .get(`/clusters/test-cluster${testPath}`)
+      .reply(200, mockResponse, { 'X-Authorization': 'newToken' });
+
+    const setTokenSpy = jest.spyOn(auth, 'setToken');
+    await apiProxy.clusterRequest(testPath, { cluster: clusterName });
+    expect(setTokenSpy).toHaveBeenCalledWith(clusterName, 'newToken');
+  });
+});
 
 describe('apiFactory', () => {
   const mockMultipleResource: [group: string, version: string, resource: string][] = [
@@ -184,7 +235,6 @@ describe('apiFactoryWithNamespace', () => {
 });
 
 describe('request, post, patch, put, delete', () => {
-  const testPath = '/test/url';
   const testData = { key: 'value' };
 
   beforeAll(() => {
