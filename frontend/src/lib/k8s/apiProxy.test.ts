3be807b960ff3654628286c1301adecebecd406d
Evangelos Skopelitis <eskopelitis@microsoft.com>
2024-04-05 13:00:56 -0400

    frontend: apiProxy.test: Add apiFactory tests
    This gives us test coverage of apiFactory and apiFactoryWithNamespace
    in apiProxy.
    
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- frontend/src/lib/k8s/apiProxy.test.ts
+++ frontend/src/lib/k8s/apiProxy.test.ts
@@ -3,15 +3,59 @@
  * https://github.com/nock/nock
  */
 
+import WS from 'jest-websocket-mock';
 import nock from 'nock';
 import * as auth from '../auth';
 import * as apiProxy from './apiProxy';
 
 const baseApiUrl = 'http://localhost';
+const wsUrl = 'ws://localhost';
 const testPath = '/test/url';
 const mockResponse = { message: 'mock response' };
 const clusterName = 'test-cluster';
+const namespace = 'default';
 const errorResponse401 = { error: 'Unauthorized', message: 'Unauthorized' };
+
+const mockSingleResource: [group: string, version: string, resource: string] = [
+  'groupA',
+  'v1',
+  'resourceA',
+];
+const mockMultipleResource: [group: string, version: string, resource: string][] = [
+  ['groupB', 'v1', 'resourceB'],
+  ['groupC', 'v1', 'resourceC'],
+  ['groupD', 'v1', 'resourceD'],
+];
+const mockConfigMap = {
+  apiVersion: 'v1',
+  kind: 'ConfigMap',
+  metadata: {
+    creationTimestamp: '2023-04-27T20:31:27Z',
+    name: 'my-pvc',
+    namespace: 'default',
+    resourceVersion: '1234',
+    uid: 'abc-1234',
+  },
+  data: {
+    storageClassName: 'default',
+    volumeMode: 'Filesystem',
+    volumeName: 'pvc-abc-1234',
+  },
+};
+const mockConfigMapList = {
+  apiVersion: 'v1',
+  items: [],
+  kind: 'ConfigMapList',
+  metadata: {
+    resourceVersion: '1234',
+    selfLink: '/api/v1/namespaces/default/configmaps',
+  },
+};
+const modifiedConfigMap = {
+  ...mockConfigMap,
+  metadata: { ...mockConfigMap.metadata, resourceVersion: '5678' },
+  data: { ...mockConfigMap.data, volumeName: 'pvc-xyz-5678' },
+};
 
 describe('apiProxy', () => {
   describe('clusterRequest', () => {
@@ -59,6 +103,151 @@ describe('apiProxy', () => {
       const setTokenSpy = jest.spyOn(auth, 'setToken');
       await apiProxy.clusterRequest(testPath, { cluster: clusterName });
       expect(setTokenSpy).toHaveBeenCalledWith(clusterName, 'newToken');
+    });
+  });
+
+  describe('apiFactory, apiFactoryWithNamespace', () => {
+    const mockServers: any = {};
+    let mockServer: WS;
+    let cb: jest.Mock;
+    let errCb: jest.Mock;
+
+    beforeEach(() => {
+      nock(baseApiUrl)
+        .persist()
+        .get(
+          uri =>
+            uri.includes(mockSingleResource[2]) ||
+            uri.includes(mockMultipleResource[0][2]) ||
+            uri.includes(mockMultipleResource[1][2]) ||
+            uri.includes(mockMultipleResource[2][2])
+        )
+        .reply(200, mockConfigMapList);
+
+      cb = jest.fn();
+      errCb = jest.fn();
+
+      jest.spyOn(console, 'warn').mockImplementation();
+    });
+
+    afterEach(() => {
+      nock.cleanAll();
+      WS.clean();
+      jest.restoreAllMocks();
+    });
+
+    describe('singleApiFactory', () => {
+      it('Successfully creates API client for single resource', done => {
+        expect.assertions(3);
+
+        mockServer = new WS(
+          `${wsUrl}/clusters/${clusterName}/apis/${mockSingleResource[0]}/${mockSingleResource[1]}/${mockSingleResource[2]}`
+        );
+
+        const client = apiProxy.apiFactory(...mockSingleResource);
+        client.list(cb, errCb, {}, clusterName);
+
+        mockServer.on('connection', async (socket: any) => {
+          expect(cb).toHaveBeenNthCalledWith(1, []);
+
+          socket.send(JSON.stringify({ type: 'ADDED', object: mockConfigMap }));
+          expect(cb).toHaveBeenNthCalledWith(2, [{ actionType: 'ADDED', ...mockConfigMap }]);
+
+          socket.send(JSON.stringify({ type: 'MODIFIED', object: modifiedConfigMap }));
+          expect(cb).toHaveBeenNthCalledWith(3, [{ actionType: 'MODIFIED', ...modifiedConfigMap }]);
+
+          done();
+        });
+      });
+    });
+
+    describe('multipleApiFactory', () => {
+      it('Successfully creates API client for multiple resources', async () => {
+        expect.assertions(10);
+
+        const connections = mockMultipleResource.map(([group, version, resource]) => {
+          return new Promise<void>(resolve => {
+            mockServers[resource] = new WS(
+              `${wsUrl}/clusters/${clusterName}/apis/${group}/${version}/${resource}`
+            );
+            const client = apiProxy.apiFactory(group, version, resource);
+            client.list(cb, errCb, {}, clusterName);
+
+            mockServers[resource].on('connection', async (socket: any) => {
+              expect(cb).toHaveBeenCalledWith([]);
+
+              socket.send(JSON.stringify({ type: 'ADDED', object: mockConfigMap }));
+              expect(cb).toHaveBeenCalledWith([{ actionType: 'ADDED', ...mockConfigMap }]);
+
+              socket.send(JSON.stringify({ type: 'MODIFIED', object: modifiedConfigMap }));
+              expect(cb).toHaveBeenCalledWith([{ actionType: 'MODIFIED', ...modifiedConfigMap }]);
+
+              resolve();
+            });
+          });
+        });
+
+        await Promise.all(connections);
+        expect(cb).toHaveBeenCalledTimes(3 * mockMultipleResource.length);
+      });
+    });
+
+    describe('simpleApiFactoryWithNamespace', () => {
+      it('Successfully creates API client for single namespaced resource', done => {
+        expect.assertions(4);
+
+        mockServer = new WS(
+          `${wsUrl}/clusters/${clusterName}/apis/${mockSingleResource[0]}/${mockSingleResource[1]}/namespaces/${namespace}/${mockSingleResource[2]}`
+        );
+
+        const client = apiProxy.apiFactoryWithNamespace(...mockSingleResource);
+        expect(client.isNamespaced).toBe(true);
+
+        client.list(namespace, cb, errCb, {}, clusterName);
+
+        mockServer.on('connection', async (socket: any) => {
+          expect(cb).toHaveBeenNthCalledWith(1, []);
+
+          socket.send(JSON.stringify({ type: 'ADDED', object: mockConfigMap }));
+          expect(cb).toHaveBeenNthCalledWith(2, [{ actionType: 'ADDED', ...mockConfigMap }]);
+
+          socket.send(JSON.stringify({ type: 'MODIFIED', object: modifiedConfigMap }));
+          expect(cb).toHaveBeenNthCalledWith(3, [{ actionType: 'MODIFIED', ...modifiedConfigMap }]);
+
+          done();
+        });
+      });
+    });
+
+    describe('multipleApiFactoryWithNamespace', () => {
+      it('Successfully creates API client for multiple namespaced resources', async () => {
+        expect.assertions(10);
+
+        const connections = mockMultipleResource.map(([group, version, resource]) => {
+          return new Promise<void>(resolve => {
+            mockServers[resource] = new WS(
+              `${wsUrl}/clusters/${clusterName}/apis/${group}/${version}/namespaces/${namespace}/${resource}`
+            );
+            const client = apiProxy.apiFactoryWithNamespace(group, version, resource);
+            client.list(namespace, cb, errCb, {}, clusterName);
+
+            mockServers[resource].on('connection', async (socket: any) => {
+              expect(cb).toHaveBeenCalledWith([]);
+
+              socket.send(JSON.stringify({ type: 'ADDED', object: mockConfigMap }));
+              expect(cb).toHaveBeenCalledWith([{ actionType: 'ADDED', ...mockConfigMap }]);
+
+              socket.send(JSON.stringify({ type: 'MODIFIED', object: modifiedConfigMap }));
+              expect(cb).toHaveBeenCalledWith([{ actionType: 'MODIFIED', ...modifiedConfigMap }]);
+
+              resolve();
+            });
+          });
+        });
+
+        await Promise.all(connections);
+        expect(cb).toHaveBeenCalledTimes(3 * mockMultipleResource.length);
+      });
     });
   });
 });
