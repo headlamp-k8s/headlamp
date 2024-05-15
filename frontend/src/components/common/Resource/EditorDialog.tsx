df048cdad431fea923c4d134c869ee063007a608
Evangelos Skopelitis <eskopelitis@microsoft.com>
2024-05-15 13:41:03 -0400

    frontend: Add placeholder text in EditorDialog
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- frontend/src/components/common/Resource/EditorDialog.tsx
+++ frontend/src/components/common/Resource/EditorDialog.tsx
@@ -119,6 +119,9 @@ export default function EditorDialog(props: EditorDialogProps) {
   // Update the code when the item changes, but only if the code hasn't been touched.
   React.useEffect(() => {
     if (!item || Object.keys(item || {}).length === 0) {
+      const defaultCode = '# Enter your YAML or JSON here';
+      originalCodeRef.current = { code: defaultCode, format: 'yaml' };
+      setCode({ code: defaultCode, format: 'yaml' });
       return;
     }
 
