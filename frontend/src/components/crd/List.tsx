e4ecdf472a71ce69bbf5f304e9ceac35ef48623a
Joaquim Rocha <joaquim.rocha@microsoft.com>
2024-05-27 10:00:28 +0100

    frontend crd/List: Add categories column
    So users can view and filter by categories.
    
    Co-authored-by: guilhane <guilhane.bourgoin@orange.com>
    Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

--- frontend/src/components/crd/List.tsx
+++ frontend/src/components/crd/List.tsx
@@ -1,22 +1,40 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import CRD from '../../lib/k8s/crd';
-import { Link } from '../common';
+import { Link, useThrottle } from '../common';
 import ResourceListView from '../common/Resource/ResourceListView';
 
 export default function CustomResourceDefinitionList() {
   const { t } = useTranslation(['glossary', 'frequent']);
+  const [items, error] = CRD.useList();
+  const throttledItems = useThrottle(items, 1000);
+
+  const categories = React.useMemo(() => {
+    if (!items || items?.length === 0) {
+      return [];
+    }
+
+    const categories = new Set<string>();
+    items.forEach((crd: CRD) => {
+      const crdCategories = crd.getCategories() || [];
+      crdCategories.forEach(category => categories.add(category));
+    });
+
+    return Array.from(categories).sort();
+  }, [items]);
 
   return (
     <ResourceListView
       title={t('glossary|Custom Resources')}
       headerProps={{
-        noNamespaceFilter: true,
+        noNamespaceFilter: false,
       }}
-      resourceClass={CRD}
+      data={throttledItems}
+      errorMessage={CRD.getErrorMessage(error)}
       columns={[
         {
           label: t('glossary|Resource'),
-          getValue: crd => crd.spec.names.kind,
+          getValue: (crd: CRD) => crd.spec.names.kind,
           render: crd => (
             <Link
               routeName="customresources"
@@ -49,6 +67,12 @@ export default function CustomResourceDefinitionList() {
         {
           label: t('Scope'),
           getValue: crd => crd.spec.scope,
+        },
+        {
+          label: t('translation|Categories'),
+          getValue: crd => crd.getCategories()?.join(', '),
+          filterVariant: 'multi-select',
+          filterSelectOptions: categories,
         },
         'age',
       ]}
