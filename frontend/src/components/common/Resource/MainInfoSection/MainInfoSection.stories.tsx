4fd8dc68b62e33dae839398ffa8fc8ae67aeb448
Joaquim Rocha <joaquim.rocha@microsoft.com>
2024-06-14 22:06:21 +0100

    frontend: Add story for MainInfoSection
    Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

---
+++ frontend/src/components/common/Resource/MainInfoSection/MainInfoSection.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, Story } from '@storybook/react';
+import Pod from '../../../../lib/k8s/pod';
+import { TestContext } from '../../../../test';
+import { podList } from '../../../pod/storyHelper';
+import { MainInfoSection, MainInfoSectionProps } from './MainInfoSection';
+
+const resource = new Pod(podList[0]);
+
+export default {
+  title: 'Resource/MainInfoSection',
+  component: MainInfoSection,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<MainInfoSectionProps> = (args: MainInfoSectionProps) => (
+  <TestContext>
+    <MainInfoSection {...args} />
+  </TestContext>
+);
+
+export const Normal = Template.bind({});
+Normal.args = {
+  resource,
+  title: 'Simple Resource',
+};
+
+export const NullBacklink = Template.bind({});
+NullBacklink.args = {
+  resource,
+  backLink: null,
+  title: 'No Back Link Resource',
+};
