033a669ac1927163ecfd587e3a3b1dc8d3c89762
Joaquim Rocha <joaquim.rocha@microsoft.com>
2024-06-20 14:05:15 +0100

    frontend: Fix top bar size in small screens
    The top bar was getting a very big margin as a regression after the
    MUIv5 styling.
    
    Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

--- frontend/src/components/App/TopBar.tsx
+++ frontend/src/components/App/TopBar.tsx
@@ -400,7 +400,7 @@ export function PureTopBar({
       >
         <Toolbar
           sx={{
-            [theme.breakpoints.down('xs')]: {
+            [theme.breakpoints.down('sm')]: {
               paddingLeft: 0,
               paddingRight: 0,
             },
