2e52f4f0976ccfd4606accd8d1685e949e49f480
René Dudfield <renedudfield@microsoft.com>
2024-07-15 16:43:38 +0200

    frontend: ReleaseNotes: Update stories to new component format
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

--- frontend/src/components/common/ReleaseNotes/UpdatePopup.stories.tsx
+++ frontend/src/components/common/ReleaseNotes/UpdatePopup.stories.tsx
@@ -1,25 +1,19 @@
-import { Meta, Story } from '@storybook/react';
 import UpdatePopup from './UpdatePopup';
 
 export default {
   title: 'common/ReleaseNotes/UpdatePopup',
   component: UpdatePopup,
   argTypes: {},
-} as Meta;
-
-const Template: Story<{
-  releaseDownloadURL?: string | null;
-  fetchingRelease?: boolean;
-  releaseFetchFailed?: boolean;
-  skipUpdateHandler: () => void;
-}> = args => <UpdatePopup {...args} />;
-
-export const Show = Template.bind({});
-Show.args = {
-  releaseDownloadURL: 'https://example.com',
 };
 
-export const Closed = Template.bind({});
-Closed.args = {
-  releaseDownloadURL: 'https://example.com',
+export const Show = {
+  args: {
+    releaseDownloadURL: 'https://example.com',
+  },
+};
+
+export const Closed = {
+  args: {
+    releaseDownloadURL: 'https://example.com',
+  },
 };
