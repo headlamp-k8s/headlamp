9c7fd9e50c03fac3a1fa3aa88830aaa3dd45a065
Vincent T <vtaylor@microsoft.com>
2024-09-16 12:12:48 -0400

    frontend: Refactor namespace playwright for app mode
    Signed-off-by: Vincent T <vtaylor@microsoft.com>

--- e2e-tests/tests/namespaces.spec.ts
+++ e2e-tests/tests/namespaces.spec.ts
@@ -5,7 +5,23 @@ import { NamespacesPage } from './namespacesPage';
 test('create a namespace with the minimal editor then delete it', async ({ page }) => {
   const name = 'testing-e2e';
   const headlampPage = new HeadlampPage(page);
+  // If we are running in cluster, we need to authenticate
+  // if (process.env.PLAYWRIGHT_TEST_MODE === 'incluster') {
+  //   await headlampPage.authenticate();
+  //   // If there's no namespaces permission, then we return
+  //   const content = await page.content();
+  //   if (!content.includes('Namespaces') || !content.includes('href="/c/main/namespaces')) {
+  //     return;
+  //   }
+  // }
+
   await headlampPage.authenticate();
+
+  await headlampPage.startFromMainPage();
+
+  const namespacesPage = new NamespacesPage(page);
+
+  await namespacesPage.navigateToNamespaces();
 
   // If there's no namespaces permission, then we return
   const content = await page.content();
@@ -13,8 +29,9 @@ test('create a namespace with the minimal editor then delete it', async ({ page 
     return;
   }
 
-  const namespacesPage = new NamespacesPage(page);
-  await namespacesPage.navigateToNamespaces();
   await namespacesPage.createNamespace(name);
+
   await namespacesPage.deleteNamespace(name);
 });
+
+// to do: add test for 'create a namespace with the create namespace button then delete it'
