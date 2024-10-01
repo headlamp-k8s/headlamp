3c2c06d28550435d540afe8164fedcb34313dc72
René Dudfield <renedudfield@microsoft.com>
2024-10-01 10:26:01 +0200

    frontend: Move vitest to a dev dependency, add vitest.config.ts
    Moving the vitest config to a separate file from the vite.config.ts
    so that prod only builds can work without vitest being installed.
    
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

---
+++ frontend/vitest.config.ts
@@ -0,0 +1,35 @@
+/// <reference types="vitest" />
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { coverageConfigDefaults } from 'vitest/config';
+import viteConfig from './vite.config'
+
+export default mergeConfig(viteConfig, defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    env: {
+      UNDER_TEST: 'true',
+    },
+    alias: [
+      {
+        find: /^monaco-editor$/,
+        replacement: __dirname + '/node_modules/monaco-editor/esm/vs/editor/editor.api',
+      },
+    ],
+
+    coverage: {
+      provider: 'istanbul',
+      reporter: [['text', { maxCols: 200 }]],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'node_modules/**',
+        'build/**',
+        'src/**/*.stories*.{js,jsx,ts,tsx}',
+      ],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+    },
+    restoreMocks: false,
+    setupFiles: ['./src/setupTests.ts'],
+  },  
+}))
+
