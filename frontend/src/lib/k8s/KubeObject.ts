2e505f8e33a101bcf39eab220674cb1fabe82ec4
René Dudfield <renedudfield@microsoft.com>
2024-11-04 13:49:34 +0100

    frontend: KubeObject: Add cluster query param to getDetailsLink
    This allows links to Details views to have the cluster from where
    the resource lives in when in multi cluster mode. This allows
    the URL to contain the multiple clusters and for the URL to still
    know which of the multiple clusters to look.

--- frontend/src/lib/k8s/KubeObject.ts
+++ frontend/src/lib/k8s/KubeObject.ts
@@ -117,7 +117,7 @@ export class KubeObject<T extends KubeObjectInterface | KubeEvent = any> {
       namespace: this.getNamespace(),
       name: this.getName(),
     };
-    const link = createRouteURL(this.detailsRoute, params);
+    const link = createRouteURL(this.detailsRoute, params, { cluster: this.cluster });
     return link;
   }
 
