ca67d8cc849e65766367b923cdf52bf7cd3f896e
Vincent T <vtaylor@microsoft.com>
2024-11-07 15:41:27 -0500

    frontend: Add a11y playwright
    Signed-off-by: Vincent T <vtaylor@microsoft.com>

--- e2e-tests/tests/namespaces.spec.ts
+++ e2e-tests/tests/namespaces.spec.ts
@@ -1,4 +1,5 @@
-import { test } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright'; // 1
+import {test } from '@playwright/test';
 import { HeadlampPage } from './headlampPage';
 import { NamespacesPage } from './namespacesPage';
 
@@ -17,4 +18,27 @@ test('create a namespace with the minimal editor then delete it', async ({ page 
   await namespacesPage.navigateToNamespaces();
   await namespacesPage.createNamespace(name);
   await namespacesPage.deleteNamespace(name);
+});
+
+test.describe('namespace dashboard', () => {
+  // 2
+  test.setTimeout(0);
+  test('create a namespace with the minimal editor then delete it', async ({ page }) => {
+    const headlampPage = new HeadlampPage(page);
+    // const namespacesPage = new NamespacesPage(page);
+
+    await headlampPage.authenticate();
+
+    const namespacesPage = new NamespacesPage(page);
+    await namespacesPage.navigateToNamespaces();
+
+    // await namespacesPage.navigateToNamespaces();
+    // await headlampPage.navigateTopage('/c/minikube/namespaces');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); // 4
+
+    console.log('accessibilityScanResults: ', accessibilityScanResults); // 5
+
+    // expect(accessibilityScanResults.violations).toEqual([]); // 5
+  });
 });
