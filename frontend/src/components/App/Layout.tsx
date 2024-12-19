f0e35fa19fccbf563a7b0409c0681379505668d5
Evangelos Skopelitis <eskopelitis@microsoft.com>
2024-12-19 17:58:02 -0500

    frontend: Layout: Display main scroll bar under top bar
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- frontend/src/components/App/Layout.tsx
+++ frontend/src/components/App/Layout.tsx
@@ -2,7 +2,6 @@ import { Box, Button } from '@mui/material';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Link from '@mui/material/Link';
-import { useTheme } from '@mui/material/styles';
 import { styled } from '@mui/material/styles';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -103,7 +102,6 @@ export default function Layout({}: LayoutProps) {
   const isFullWidth = useTypedSelector(state => state.ui.isFullWidth);
   const { t } = useTranslation();
   const allClusters = useClustersConf();
-  const theme = useTheme();
 
   /** This fetches the cluster config from the backend and updates the redux store on an interval.
    * When stateless clusters are enabled, it also fetches the stateless cluster config from the
@@ -209,33 +207,54 @@ export default function Layout({}: LayoutProps) {
       >
         {t('Skip to main content')}
       </Link>
-      <Box sx={{ display: 'flex', [theme.breakpoints.down('sm')]: { display: 'block' } }}>
-        <VersionDialog />
-        <CssBaseline enableColorScheme />
+      <VersionDialog />
+      <CssBaseline enableColorScheme />
+      <ActionsNotifier />
+      <Box
+        sx={{
+          display: 'flex',
+          overflow: 'auto',
+          flexDirection: 'column',
+          height: '100dvh',
+        }}
+      >
         <TopBar />
-        <Sidebar />
-        <Main id="main" sx={{ flexGrow: 1, marginLeft: 'initial' }}>
-          {clustersNotInURL.slice(0, MAXIMUM_NUM_ALERTS).map(clusterName => (
-            <ClusterNotFoundPopup key={clusterName} cluster={clusterName} />
-          ))}
-          <AlertNotification />
-          <Box>
-            <Div sx={theme.mixins.toolbar} />
-            <Container {...containerProps}>
-              <NavigationTabs />
-              {arePluginsLoaded && (
-                <RouteSwitcher
-                  requiresToken={() => {
-                    const clusterName = getCluster() || '';
-                    const cluster = clusters ? clusters[clusterName] : undefined;
-                    return cluster?.useToken === undefined || cluster?.useToken;
-                  }}
-                />
-              )}
-            </Container>
-          </Box>
-        </Main>
-        <ActionsNotifier />
+        <Box
+          sx={{
+            display: 'flex',
+            overflow: 'hidden',
+          }}
+        >
+          <Sidebar />
+          <Main
+            id="main"
+            sx={{
+              flexGrow: 1,
+              marginLeft: 'initial',
+              overflow: 'auto',
+            }}
+          >
+            {clustersNotInURL.slice(0, MAXIMUM_NUM_ALERTS).map(clusterName => (
+              <ClusterNotFoundPopup key={clusterName} cluster={clusterName} />
+            ))}
+            <AlertNotification />
+            <Box>
+              <Div />
+              <Container {...containerProps}>
+                <NavigationTabs />
+                {arePluginsLoaded && (
+                  <RouteSwitcher
+                    requiresToken={() => {
+                      const clusterName = getCluster() || '';
+                      const cluster = clusters ? clusters[clusterName] : undefined;
+                      return cluster?.useToken === undefined || cluster?.useToken;
+                    }}
+                  />
+                )}
+              </Container>
+            </Box>
+          </Main>
+        </Box>
       </Box>
     </>
   );
