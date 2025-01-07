2d209377f84b8774b56103180dc51d1085137b11
Yurii Vlasov <yuriy@vlasov.pro>
2025-01-07 14:08:54 +0200

    Rolling back TooltipLight changes.
    Signed-off-by: Yurii Vlasov <yuriy@vlasov.pro>

--- frontend/src/components/common/Tooltip/TooltipLight.tsx
+++ frontend/src/components/common/Tooltip/TooltipLight.tsx
@@ -35,8 +35,10 @@ export default function TooltipLight(props: TooltipLightProps) {
 
   return (
     <Tooltip
-      {...rest}
-      disableInteractive={disableInteractive}
+      {...props}
+      // children prop in the mui Tooltip is defined as ReactElement which is not totally correct
+      // string should be a valid child and is used a lot in this project
+      // but it's not included in the ReactElement type
       children={props.children as unknown as ReactElement}
     />
   );
