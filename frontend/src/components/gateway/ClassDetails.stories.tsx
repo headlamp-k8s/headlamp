415d00f2ec71636596acddded3a72256f88d9b3c
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-01-17 18:01:49 +0100

    frontend: Update mocks for Gateway Class Details
    Signed-off-by: Oleksandr Dubenko <oldubenko@microsoft.com>

--- frontend/src/components/gateway/ClassDetails.stories.tsx
+++ frontend/src/components/gateway/ClassDetails.stories.tsx
@@ -24,6 +24,10 @@ export default {
           http.get('http://localhost:4466/apis/gateway.networking.k8s.io/v1/gatewayclasses', () =>
             HttpResponse.error()
           ),
+          http.get(
+            'http://localhost:4466/apis/gateway.networking.k8s.io/v1beta1/gatewayclasses',
+            () => HttpResponse.error()
+          ),
           http.get('http://localhost:4466/api/v1/namespaces/default/events', () =>
             HttpResponse.json({
               kind: 'EventList',
