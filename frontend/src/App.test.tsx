a5b33e0ddc1a1c064f14696c9bb64a2370b34047
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-01-24 13:16:05 +0100

    frontend: Reenable App test
    Signed-off-by: Oleksandr Dubenko <oldubenko@microsoft.com>

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,7 @@ import { render, waitFor } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-// todo: enable this after https://github.com/vitest-dev/vitest/issues/4143 is fixed
-test.skip('renders without crashing', async () => {
+test('renders without crashing', async () => {
   const { getByText } = render(
     <React.Suspense fallback="Loading...">
       <App />
