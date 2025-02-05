1ecbcb29351bd4fd2d7eccaf11bc32aafe37547e
adwait-godbole <adwaitngodbole@gmail.com>
2025-02-05 20:45:02 +0530

    frontend: Add restart bulk action for resources list
    Signed-off-by: adwait-godbole <adwaitngodbole@gmail.com>

--- frontend/src/components/common/Resource/ResourceTableMultiActions.tsx
+++ frontend/src/components/common/Resource/ResourceTableMultiActions.tsx
@@ -3,6 +3,8 @@ import { MRT_TableInstance } from 'material-react-table';
 import { useCallback } from 'react';
 import { KubeObject } from '../../../lib/k8s/KubeObject';
 import DeleteMultipleButton from './DeleteMultipleButton';
+import { isRestartableResource } from './RestartButton';
+import RestartMultipleButton from './RestartMultipleButton';
 
 export interface ResourceTableMultiActionsProps<RowItem extends Record<string, any>> {
   table: MRT_TableInstance<RowItem>;
@@ -12,14 +14,22 @@ export default function ResourceTableMultiActions<RowItem extends Record<string,
   props: ResourceTableMultiActionsProps<RowItem>
 ) {
   const { table } = props;
+
   const items = table.getSelectedRowModel().rows.map(t => t.original as unknown as KubeObject);
+  const restartableItems = items.filter(isRestartableResource);
 
   const afterConfirm = useCallback(() => {
     table.resetRowSelection();
   }, [table]);
+
   return (
-    <Grid item>
-      <Grid item container alignItems="center" justifyContent="flex-end">
+    <Grid container spacing={2}>
+      {restartableItems.length > 0 && (
+        <Grid item>
+          <RestartMultipleButton items={restartableItems} afterConfirm={afterConfirm} />
+        </Grid>
+      )}
+      <Grid item>
         <DeleteMultipleButton items={items} afterConfirm={afterConfirm} />
       </Grid>
     </Grid>
