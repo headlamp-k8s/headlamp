deac19f2514eae4d360a76061a0b629f060b3720
Oleksandr Dubenko <oldubenko@microsoft.com>
2025-02-14 17:11:54 +0100

    plugins: Remove webpack, storyshots and use vite to build plugins
    Signed-off-by: Oleksandr Dubenko <oldubenko@microsoft.com>

--- plugins/headlamp-plugin/config/.storybook/main.js
+++ plugins/headlamp-plugin/config/.storybook/main.js
@@ -1,47 +1,24 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   stories: [
     '../../../../../src/**/*.stories.mdx',
     '../../../../../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    // '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-vite',
     disableTelemetry: true,
   },
-  framework: '@storybook/react',
-
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: false,
-  },
-
-  webpackFinal: async config => {
-    let newConfig = {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        fallback: {
-          ...config.fallback,
-          crypto: require.resolve('crypto-browserify'),
-          http: require.resolve('stream-http'),
-          https: require.resolve('https-browserify'),
-          path: require.resolve('path-browserify'),
-          stream: require.resolve('stream-browserify'),
-          vm: require.resolve('vm-browserify'),
-        },
-      },
-    };
-
-    // To find and use the tsconfig inside the plugin.
-    newConfig.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
-
-    return newConfig;
+    reactDocgen: true,
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: prop => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
   },
 };
--- plugins/headlamp-plugin/template/src/storybook.test.tsx
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-import { initTests } from '@kinvolk/headlamp-plugin/config/storyshots/storyshots-test';
-
-// Run the *.stories.tsx files in here as storyshot tests.
-initTests();
