f08a522af9e7eb8f9374b54e715cd34cb55657af
Jineshbansal <732005jinesh@gmail.com>
2025-02-17 23:16:47 +0530

    frontend: tsdocs: remove types in comments from ThemeProviderNexti18n.tsx and LocaleSelect.tsx
    Signed-off-by: Jineshbansal <732005jinesh@gmail.com>

--- frontend/src/i18n/ThemeProviderNexti18n.tsx
+++ frontend/src/i18n/ThemeProviderNexti18n.tsx
@@ -10,12 +10,12 @@ import { Loader } from '../components/common';
  * @remarks
  * This method retrieves the locale configuration for a specific language supported by
  * Material UI. It checks the provided language code against a predefined set of available locales,
- * and returns the corresponding locale object. If the language code is not found, it defaults to 
+ * and returns the corresponding locale object. If the language code is not found, it defaults to
  * English (`enUS`).
  *
  * @param locale - The language code for which the locale object is required (e.g., 'en', 'pt', 'es').
- * 
- * @returns The Material UI locale object corresponding to the provided language code. 
+ *
+ * @returns The Material UI locale object corresponding to the provided language code.
  *          Defaults to `enUS` if the locale is not found in the predefined list.
  */
 
@@ -36,20 +36,20 @@ function getLocale(locale: string): typeof enUS {
 /**
  * A custom theme provider component that integrates Material UI's theme with `react-i18next` for language selection.
  * It ensures that both the theme and language are synchronized, providing seamless localization across the application.
- * 
+ *
  * @remarks
- * This component functions like a standard Material UI `ThemeProvider`, but it also listens for language changes using `react-i18next`. 
+ * This component functions like a standard Material UI `ThemeProvider`, but it also listens for language changes using `react-i18next`.
  * When the language is changed, the corresponding Material UI locale is applied to update the theme accordingly.
  * This is essential for supporting multiple languages and ensuring the UI behaves correctly with Right-To-Left (RTL) languages.
- * 
+ *
  * The component will show a loading state (`<Loader />`) until the i18n resources are ready for rendering.
- * 
- * @param {object} props - The properties for the component.
- * @param {Theme} props.theme - The Material UI theme object to be applied to the app.
- * @param {ReactNode} props.children - The child components wrapped by this theme provider.
- * 
- * @returns {ReactNode} The children components wrapped with the updated Material UI theme and language configurations.
- * 
+ *
+ * @param props - The properties for the component.
+ * @param props.theme - The Material UI theme object to be applied to the app.
+ * @param props.children - The child components wrapped by this theme provider.
+ *
+ * @returns The children components wrapped with the updated Material UI theme and language configurations.
+ *
  * @example
  * ```tsx
  * <ThemeProviderNexti18n theme={theme}>
